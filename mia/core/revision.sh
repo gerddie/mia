7739258e6c6b8e1ae0cce842cb1b4da17468cfdc
Gert Wollny <wollny@sisifo.die.upm.es>
2010-03-24 16:26:10 +0100

    change revision script to use GIT

--- mia/core/revision.sh
+++ mia/core/revision.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# only use this if we are in a subversion repository
-
+# only use this if we are in a git repository
 
 [ -e ~/.profile ] && . ~/.profile
 
-if [ -d $1/.svn ]; then
-    pushd $1 
-    rev=`svn info | grep Revision`
+if [ -d $1/.git ]; then
+    rev=`git describe`
     val="#define LIBMIA_REVISION "\"$rev\"
     echo $val >revision.hh.new
     if [ -e revision.hh ] ; then 
@@ -18,11 +16,10 @@ if [ -d $1/.svn ]; then
     else 
 	   mv revision.hh.new revision.hh
     fi 
-    popd 
 elif [ ! -f $1/revision.hh ] ; then 
-    echo "WARNING: Unable to obtain revision number from subversion tree"
-    echo "WARNING: Either you're not using a working copy of the svn version"
-    echo "WARNING: or the subversion client is not available"
+    echo "WARNING: Unable to obtain revision info from git tree"
+    echo "WARNING: Either you're not using a working copy of the git version"
+    echo "WARNING: or the git  client is not available"
     echo "WARNING: As a workaround you may create a 'revision.hh' by hand in the <source root>/mia/core directory"
     echo "WARNING: with the line '#define LIBMIA_REVISION \"unknown\"'"
     echo "WARNING: otherwise build will fail with 'revision.cc: revision.hh: No such file or directory'"
