381ef398581c518d09f5dd1403c9ee78e417fc6f
Gert Wollny <gw.fossdev@gmail.com>
2010-08-10 23:24:47 +0200

    add 1D scaler

---
+++ mia/core/test_scaler1d.cc
@@ -0,0 +1,141 @@
+/* -*- mia-c++  -*-
+ *
+ * Copyright (c) Leipzig, Madrid 2004-2010
+ * Max-Planck-Institute for Human Cognitive and Brain Science
+ * Max-Planck-Institute for Evolutionary Anthropology
+ * BIT, ETSI Telecomunicacion, UPM
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PUcRPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <stdexcept>
+#include <climits>
+#include <cmath>
+
+#include <mia/internal/autotest.hh>
+
+#include <boost/filesystem/path.hpp>
+
+#include <mia/core/spacial_kernel.hh>
+#include <mia/core/cmdlineparser.hh>
+#include <mia/core/msgstream.hh>
+
+
+#include <mia/core/scaler1d.hh>
+
+
+NS_MIA_USE
+using namespace std;
+using namespace boost::unit_test;
+namespace bfs = ::boost::filesystem;
+
+struct Scaler1DFixture  {
+	Scaler1DFixture(); 
+
+	double f(double x) const;
+	void test_size(EInterpolation type, size_t target_size, double tolerance = 1);
+	gsl::DoubleVector data; 
+};
+
+
+BOOST_FIXTURE_TEST_CASE( test_bspline2_upscale, Scaler1DFixture)
+{
+	test_size(ip_bspline2, 500);
+}
+
+BOOST_FIXTURE_TEST_CASE( test_bspline2_downscale, Scaler1DFixture)
+{
+	test_size(ip_bspline2, 130);
+}
+
+BOOST_FIXTURE_TEST_CASE( test_bspline3_upscale, Scaler1DFixture)
+{
+	test_size(ip_bspline3, 500);
+}
+
+BOOST_FIXTURE_TEST_CASE( test_bspline3_downscale, Scaler1DFixture)
+{
+	test_size(ip_bspline3, 130);
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_bspline4_upscale, Scaler1DFixture)
+{
+	test_size(ip_bspline4, 500);
+}
+
+BOOST_FIXTURE_TEST_CASE( test_bspline4_downscale, Scaler1DFixture)
+{
+	test_size(ip_bspline4, 130);
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_bspline5_upscale, Scaler1DFixture)
+{
+	test_size(ip_bspline5, 500);
+}
+
+BOOST_FIXTURE_TEST_CASE( test_bspline5_downscale, Scaler1DFixture)
+{
+	test_size(ip_bspline5, 130);
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_omoms3_upscale, Scaler1DFixture)
+{
+	test_size(ip_omoms3, 500);
+}
+BOOST_FIXTURE_TEST_CASE( test_omoms3_downscale, Scaler1DFixture)
+{
+	test_size(ip_omoms3, 130);
+}
+
+
+double Scaler1DFixture::f(double x) const
+{
+	return cos(x);
+}
+
+Scaler1DFixture::Scaler1DFixture():
+	data(256)
+{
+	for(size_t x = 0; x < 256; ++x)
+		data[x] = f(2 * M_PI * x / 255.0);
+}
+
+void Scaler1DFixture::test_size(EInterpolation type, size_t target_size, double tolerance)
+{
+	gsl::DoubleVector result(target_size); 
+	
+	unique_ptr<C1DInterpolatorFactory>  ipf(create_1dinterpolation_factory(type));	
+	C1DScalarFixed scaler(*ipf->get_kernel(), data.size(), target_size); 
+	
+	scaler(data, result); 
+
+	for(size_t i = 0; i < target_size; ++i) {
+		double x = (2 * M_PI * i) / (target_size - 1); 
+		double fx = f(x); 
+		cvdebug()  << " sin("<< x << ") = " << fx 
+			   << ", interp= " << result[i] 
+			   << ", Q= " << fx/ result[i] 
+			   << "\n"; 
+
+		
+		if (abs(fx) > 0.0001 || abs(result[i]) > 0.0001) 
+			BOOST_CHECK_CLOSE( fx, result[i], 0.1);
+	}
+}
+
