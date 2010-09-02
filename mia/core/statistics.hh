5b5a47f4baec7a67174e2b716cc551982dfe4f22
Gert Wollny <gw.fossdev@gmail.com>
2010-09-02 19:14:27 +0200

    add some simple statistics functors

---
+++ mia/core/statistics.hh
@@ -0,0 +1,135 @@
+/* -*- mia-c++  -*-
+ *
+ * Copyright (c) Madrid 2010
+ * BIT, ETSI Telecomunicacion, UPM
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PUcRPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+
+#ifndef mia_core_statistics_hh
+#define mia_core_statistics_hh
+
+#include <cmath>
+#include <mia/core/filter.hh>
+
+
+/**
+   Functor to be called by mia::filter to evaluate mean and variance of 
+   a series of data. 
+   
+*/
+struct  FMeanVariance: public TFilter< pair<double, double> >  {
+
+	typedef TFilter< pair<double, double> >::result_type result_type; 
+	
+	/**
+	   \tparam T container type T holding the data to be analyzed. Must provide
+	   the methods begin() and end() returning a forward_iterator; 
+	   \param data the data to be anlyzed
+	   \returns a std::pair first=mean, second=variance
+	 */
+
+	template <typename T> 
+	result_type operator()( const T& data) const; 
+}; 
+
+/**
+   Functor to be called by mia::filter to evaluate median and median average distance (MAD) of 
+   a series of data. 
+*/
+struct  FMedianMAD: public TFilter< pair<double, double> >  {
+
+	typedef TFilter< pair<double, double> >::result_type result_type; 
+	
+	/**
+	   \tparam T container type T holding the data to be analyzed. Must provide
+	   the methods begin() and end() returning a forward_iterator; 
+	   \param data the data to be anlyzed
+	   \returns a std::pair first=median, second=MAD
+	 */
+	template <typename T> 
+	result_type operator()( const T& data) const; 
+private: 
+	double median(std::vector<double>& buf)const; 
+}; 
+
+
+
+template <typename T> 
+FMeanVariance::result_type FMeanVariance::operator()( const T& data) const
+{
+	double sum = 0.0; 
+	double sum2  = 0.0; 
+	double n = data.size(); 
+	for (auto i = data.begin(); i != data.end(); ++i) {
+		sum += *i; 
+		sum2 += *i * *i; 
+	}
+
+	FMeanVariance::result_type result = {0.0, 0.0}; 
+	
+	if (n > 0) {
+		result.first = 	sum / n;
+		if (n > 1) 
+			result.second = sqrt((sum2 - result.first * sum) / (n - 1));
+	}
+	return result; 
+}
+
+struct __deltaabs {
+	__deltaabs(double d):m_d(d){}; 
+	double operator ()(double x) const {
+		return abs(x - m_d); 
+	}
+private: 
+	double m_d; 
+}; 
+
+
+template <typename T> 
+FMedianMAD::result_type FMedianMAD::operator()( const T& data) const
+{
+	std::vector<double> buffer(data.size()); 
+	copy(data.begin(), data.end(), buffer.begin()); 
+
+	FMedianMAD::result_type result; 
+	result.first = median(buffer); 
+
+	transform(buffer.begin(), buffer.end(), buffer.begin(), __deltaabs(result.first)); 
+	result.second = median(buffer); 
+	return result; 
+}
+
+double FMedianMAD::median(std::vector<double>& buf)const
+{
+	if (buf.empty()) 
+		return 0.0; 
+
+	if (buf.size() & 1) {
+		auto i = buf.begin() + (buf.size() - 1) / 2;
+		std::nth_element(buf.begin(), i, buf.end());
+		return *i;
+	}else{
+		auto i1 = buf.begin() + buf.size() / 2 - 1;
+		auto i2 = buf.begin() + buf.size() / 2;
+		std::nth_element(buf.begin(), i1, buf.end());
+		std::nth_element(buf.begin(), i2, buf.end());
+		return (*i1 + *i2) / 2.0;
+	}
+}
+
+#endif
---
+++ mia/core/test_statistics.cc
@@ -0,0 +1,104 @@
+/* -*- mia-c++  -*-
+ *
+ * Copyright (c) Madrid 2010
+ * BIT, ETSI Telecomunicacion, UPM
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PUcRPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <mia/internal/autotest.hh>
+#include <mia/core/statistics.hh>
+
+NS_MIA_BEGIN
+class CSTDVector {
+public: 
+	EPixelType get_pixel_type() const {
+		return m_pt; 
+	}
+	virtual size_t size() const = 0; 
+protected: 
+	CSTDVector(EPixelType pt): m_pt(pt) {}
+private: 
+	EPixelType m_pt; 
+}; 
+
+template <typename T> 
+class TSTDVector: public CSTDVector {
+	
+public: 
+	TSTDVector(size_t size):
+		CSTDVector((EPixelType)pixel_type<T>::value), 
+		m_data(size){}; 
+	
+	size_t size() const {
+		return m_data.size(); 
+	} 
+	typename std::vector<T>::const_iterator begin() const {
+		return m_data.begin(); 
+	}
+	typename std::vector<T>::const_iterator end() const {
+		return m_data.end(); 
+	}
+
+	typename std::vector<T>::iterator begin() {
+		return m_data.begin(); 
+	}
+	typename std::vector<T>::iterator end() {
+		return m_data.end(); 
+	}
+
+private: 
+	std::vector<T> m_data; 
+}; 
+
+template <>
+struct Binder<CSTDVector> {
+	typedef __bind_all<TSTDVector> Derived;
+};
+NS_MIA_END
+
+NS_MIA_USE; 
+
+
+BOOST_AUTO_TEST_CASE( test_mean_var )
+{
+	const float test_data[5] = {
+		1.0, 0.0, -1.0, 2.0, 3.0
+	};
+	TSTDVector<double> data(5); 
+	copy(test_data, test_data + 5, data.begin()); 
+
+	CSTDVector& help = data; 
+	auto mean_var = mia::filter(FMeanVariance(), help); 
+	BOOST_CHECK_CLOSE(mean_var.first, 1.0, 0.1);
+	BOOST_CHECK_CLOSE(mean_var.second, 1.5811, 0.1);
+}
+
+
+BOOST_AUTO_TEST_CASE( test_median_MAD_1 )
+{
+	const float test_data[5] = {
+		1.0, 0.0, -1.0, 2.0, 3.0
+	};
+	TSTDVector<double> data(5); 
+	copy(test_data, test_data + 5, data.begin()); 
+
+	CSTDVector& help = data; 
+	auto median_MAD = mia::filter(FMedianMAD(), help); 
+	BOOST_CHECK_EQUAL(median_MAD.first, 1.0);
+	BOOST_CHECK_EQUAL(median_MAD.second, 1.0);
+}
+
