03da55e6ca04144b0675c836679911ba66665e50
Gert Wollny <gw.fossdev@gmail.com>
2011-02-28 17:44:52 +0100

    add nonrigid registration

---
+++ src/3dnonrigidreg.cc
@@ -0,0 +1,130 @@
+/* -*- mona-c++  -*-
+ *
+ * Copyright (c) Leipzig, Madrid 2004-2010
+ *
+ * Max-Planck-Institute for Human Cognitive and Brain Science
+ * Max-Planck-Institute for Evolutionary Anthropology
+ * BIT, ETSI Telecomunicacion, UPM
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PUcRPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+
+#include <sstream>
+#include <mia/core.hh>
+#include <mia/3d.hh>
+#include <mia/3d/nonrigidregister.hh>
+#include <gsl++/multimin.hh>
+#include <mia/3d/transformfactory.hh>
+#include <mia/3d/transformio.hh>
+#include <mia/core/factorycmdlineoption.hh>
+
+NS_MIA_USE;
+using namespace std;
+using namespace gsl;
+
+
+const TDictMap<EMinimizers>::Table g_minimizer_table[] = {
+	{"cg-fr", min_cg_fr},
+	{"cg-pr", min_cg_pr},
+	{"bfgs", min_bfgs},
+	{"bfgs2", min_bfgs2},
+	{"gd", min_gd},
+	{NULL, min_undefined}
+};
+
+
+const char *g_general_help = 
+	"This program runs the non-rigid registration of two images using certain"
+	"cost measures and a given transformation model.\n"
+	"Basic usage: \n"
+	" mia-3dnonrigidreg [options] cost1 cost2 "; 
+
+int do_main( int argc, const char *argv[] )
+{
+	string cost_function("ssd"); 
+	string src_filename;
+	string ref_filename;
+	string out_filename;
+	string trans_filename;
+	string transform_type("spline");
+	EMinimizers minimizer = min_bfgs2;
+
+	cvdebug() << "auto transform_creator\n"; 
+	auto transform_creator = C3DTransformCreatorHandler::instance().produce("spline"); 
+	if (!transform_creator && transform_creator->get_init_string() != string("spline"))
+		cverr() << "something's wrong\n"; 
+
+	size_t mg_levels = 3;
+
+	CCmdOptionList options(g_general_help);
+	options.push_back(make_opt( src_filename, "in", 'i', "test image", CCmdOption::required));
+	options.push_back(make_opt( ref_filename, "ref", 'r', "reference image", CCmdOption::required));
+	options.push_back(make_opt( out_filename, "out", 'o', "registered output image", CCmdOption::required));
+	options.push_back(make_opt( trans_filename, "trans", 't', "output transformation"));
+	options.push_back(make_opt( mg_levels, "levels", 'l', "multigrid levels", CCmdOption::required));
+	options.push_back(make_opt( minimizer, TDictMap<EMinimizers>(g_minimizer_table),
+				    "optimizer", 'O', "Optimizer used for minimization"));
+	options.push_back(make_opt( transform_creator, "transForm", 'f', "transformation type"));
+
+	options.parse(argc, argv);
+	
+	auto cost_descrs = options.get_remaining(); 
+
+	C3DFullCostList costs; 
+	for (auto i = cost_descrs.begin(); i != cost_descrs.end(); ++i)
+		costs.push(C3DFullCostPluginHandler::instance().produce(*i)); 
+
+	P3DImage Model = load_image<P3DImage>(src_filename);
+	P3DImage Reference = load_image<P3DImage>(ref_filename);
+	C3DBounds GlobalSize = Model->get_size();
+	if (GlobalSize != Reference->get_size())
+		throw std::invalid_argument("Images have different size");
+
+	unique_ptr<C3DInterpolatorFactory>   ipfactory(create_3dinterpolation_factory(ip_bspline3));
+	C3DNonrigidRegister nrr(costs, minimizer,  transform_creator, *ipfactory, mg_levels);
+	P3DTransformation transform = nrr.run(Model, Reference);
+	P3DImage result = (*transform)(*Model, *ipfactory);
+
+	if (!trans_filename.empty()) {
+		if (!C3DTransformationIOPluginHandler::instance().save("", trans_filename, *transform)) 
+			cverr() << "Saving the transformation to '" << trans_filename << "' failed."; 
+	}
+
+	return save_image(out_filename, result);
+}
+
+
+int main( int argc, const char *argv[] )
+{
+	try {
+		return do_main(argc, argv);
+	}
+	catch (const runtime_error &e){
+		cerr << argv[0] << " runtime: " << e.what() << endl;
+	}
+	catch (const invalid_argument &e){
+		cerr << argv[0] << " error: " << e.what() << endl;
+	}
+	catch (const exception& e){
+		cerr << argv[0] << " error: " << e.what() << endl;
+	}
+	catch (...){
+		cerr << argv[0] << " unknown exception" << endl;
+	}
+
+	return EXIT_FAILURE;
+}
