ea09522b0a159db144ed479b33fac60fd78df785
Gert Wollny <gw.fossdev@gmail.com>
2011-08-08 13:44:40 +0200

    update copyright notice

--- mia/2d/register.cc
+++ mia/2d/register.cc
@@ -1,6 +1,6 @@
 /* -*- mia-c++  -*-
  *
- * Copyright (c) Leipzig, Madrid 2004-2011
+ * Copyright (c) Leipzig, Madrid 1999-2011 Gert Wollny
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
