498881e50fe801619c068cf51e1e60ffe83f7a57
Gert Wollny <gw.fossdev@gmail.com>
2011-08-26 17:29:15 +0200

    add a 1D wavelet transform class

---
+++ gsl++/wavelet.cc
@@ -0,0 +1,106 @@
+
+/* -*- mia-c++  -*-
+ *
+ * Copyright (c) Leipzig, Madrid 1999-2011 Gert Wollny
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <stdexcept>
+#include <gsl++/wavelet.hh>
+#include <gsl/gsl_wavelet.h>
+
+using std::invalid_argument; 
+using std::vector; 
+using std::copy; 
+
+namespace gsl {
+
+
+class C1DWaveletImpl {
+public:
+	C1DWaveletImpl(EWaveletType wt, size_t k); 
+
+	~C1DWaveletImpl(); 
+
+	vector<double> forward(const vector<double>& x) const; 
+
+private: 
+	gsl_wavelet *m_wavelet;
+}; 
+
+
+C1DWavelet::C1DWavelet(EWaveletType wt, size_t k):
+	impl(new C1DWaveletImpl(wt,k))
+{
+}
+
+C1DWavelet::~C1DWavelet()
+{
+	delete impl; 
+}
+
+std::vector<double> C1DWavelet::forward(const std::vector<double>& x) const
+{
+	return impl->forward(x); 
+}
+
+C1DWaveletImpl::C1DWaveletImpl(EWaveletType wt, size_t k):
+	m_wavelet(NULL)
+{
+	switch (wt) {
+	case wt_haar:         m_wavelet = gsl_wavelet_alloc (gsl_wavelet_haar_centered, k); break; 
+	case wt_haar_centered:m_wavelet = gsl_wavelet_alloc (gsl_wavelet_haar_centered, k); break; 
+	case wt_daubechies:          m_wavelet = gsl_wavelet_alloc (gsl_wavelet_daubechies, k); break; 
+	case wt_daubechies_centered: m_wavelet = gsl_wavelet_alloc (gsl_wavelet_daubechies_centered, k); break; 
+	case wt_bspline:         m_wavelet = gsl_wavelet_alloc (gsl_wavelet_bspline_centered, k); break; 
+	case wt_bspline_centered:m_wavelet = gsl_wavelet_alloc (gsl_wavelet_bspline_centered, k); break; 
+	default:
+		throw invalid_argument("C1DWavelet: Unknown wavelet type requested");
+	}
+	if (!m_wavelet) 
+		throw invalid_argument("C1DWavelet: Unsupported member or insufficienr memory");
+}
+
+
+C1DWaveletImpl::~C1DWaveletImpl()
+{
+	gsl_wavelet_free(m_wavelet); 
+}
+
+
+
+vector<double> C1DWaveletImpl::forward(const vector<double>& x) const
+{
+	size_t s = x.size(); 
+	size_t s1 = 1; 
+	while (s / 2)
+		s1 *=2; 
+	
+	if (s1 < x.size()) 
+		s1 *=2;
+	
+	vector<double> x_size_to_pow2(s1); 
+	copy(x.begin(), x.end(), x_size_to_pow2.begin());
+
+	
+	gsl_wavelet_workspace * ws = gsl_wavelet_workspace_alloc (s1); 
+	gsl_wavelet_transform_forward (m_wavelet, &x_size_to_pow2[0], 1, s1, ws); 
+	gsl_wavelet_workspace_free (ws); 
+	return x_size_to_pow2; 
+}
+
+}
---
+++ gsl++/wavelet.hh
@@ -0,0 +1,54 @@
+/* -*- mia-c++  -*-
+ *
+ * Copyright (c) Leipzig, Madrid 1999-2011 Gert Wollny
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+
+#ifndef gslpp_wavelet_hh
+#define gslpp_wavelet_hh
+
+#include <gsl++/gsldefines.hh>
+#include <vector>
+
+namespace gsl {
+
+enum EWaveletType {
+	wt_haar, 
+	wt_haar_centered, 
+	wt_daubechies, 
+	wt_daubechies_centered, 
+	wt_bspline, 
+	wt_bspline_centered, 
+	wt_none
+}; 
+
+class EXPORT_GSL C1DWavelet {
+public:
+	C1DWavelet(EWaveletType wt, size_t k); 
+
+	~C1DWavelet(); 
+
+	std::vector<double> forward(const std::vector<double>& x) const; 
+
+private: 
+	struct C1DWaveletImpl *impl; 
+}; 
+
+}
+
+#endif
