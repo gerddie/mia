f81118c546742bfb36d2c543e71899869ea73c82
Gert Wollny <gw.fossdev@gmail.com>
2011-09-16 13:52:24 +0200

    Add an option to setthe maximum number of threads.

--- src/2dmyoica-nonrigid-parallel.cc
+++ src/2dmyoica-nonrigid-parallel.cc
@@ -319,6 +319,9 @@ int do_main( int argc, const char *argv[] )
 	size_t current_pass = 0; 
 	size_t pass = 3; 
 
+	int max_threads = task_scheduler_init::automatic;
+
+
 	CCmdOptionList options(g_general_help);
 	
 	options.set_group("File-IO"); 
@@ -372,8 +375,15 @@ int do_main( int argc, const char *argv[] )
 	options.add(make_opt(segmethod , C2DPerfusionAnalysis::segmethod_dict, "segmethod", 'E', 
 				   "Segmentation method")); 
 
+	options.set_group("Processing"); 
+	options.add(make_opt(max_threads, "threads", 't', "Maxiumum number of threads to use for running the registration," 
+			     "This number should be lower or equal to the number of processing cores in the machine"
+			     " (default: automatic estimation)."));  
+
 	if (options.parse(argc, argv, false) != CCmdOptionList::hr_no) 
 		return EXIT_SUCCESS; 
+
+	task_scheduler_init init(max_threads);
 
 	// load input data set
 	CSegSetWithImages  input_set(in_filename, override_src_imagepath);
