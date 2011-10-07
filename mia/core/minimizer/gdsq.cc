955be20d057eeed0d5fa4d1e7f80f6003150ff20
Gert Wollny <gw.fossdev@gmail.com>
2011-10-07 15:24:56 +0200

    correct behavior of optimizer

--- mia/core/minimizer/gdsq.cc
+++ mia/core/minimizer/gdsq.cc
@@ -29,14 +29,15 @@ using namespace mia;
 
 CGDSQMinimizer::CGDSQMinimizer(double start_step, double step_scale, 
 			       double xtol, double gtol, double ftolr, 
-			       unsigned int maxiter):
+			       unsigned int maxiter, double min_step):
 	
 	m_xtol( xtol ), 
 	m_gtol( gtol ), 
 	m_maxiter( maxiter ), 
 	m_step( start_step ),
 	m_step_scale(step_scale), 
-	m_ftolr(ftolr)
+	m_ftolr(ftolr),
+	m_min_step(min_step)
 {
 	add(property_gradient); 
 }
@@ -64,38 +65,44 @@ int CGDSQMinimizer::do_run(CDoubleVector& x)
 	copy(x.begin(), x.end(), xwork.begin()); 
 	
 	double f_init = get_problem().fdf(xwork, g);
+	double step = m_step; 
 	
 	double f_old = f_init; 
 	int iter = 0; 
 	int success = test_tol(g, m_xtol, SUCCESS_XTOLA);
-	while (iter++ < m_maxiter && !success) {
-		cblas_daxpy(g.size(), -m_step, &g[0], 1, &xwork[0], 1);
+	while (iter++ < m_maxiter && step > m_min_step && !success) {
+		cblas_daxpy(g.size(), -step, &g[0], 1, &xwork[0], 1);
 		double f = get_problem().fdf(xwork, gt);
 
 		if (f < f_old) {
-			cblas_dscal(g.size(), -m_step, &g[0], 1);
+			cblas_dscal(g.size(), -step, &g[0], 1);
 			double sum = cblas_ddot(g.size(), &g[0], 1, &gt[0], 1); 
 			double aux = f_old - f + sum;
 			if (aux <= 0.0) 
-				m_step *= m_step_scale;
+				step *= m_step_scale;
 			else {
-				m_step = 0.5 * cblas_dnrm2(g.size(), &g[0], 1) / aux; 
+				step = 0.5 * cblas_dnrm2(g.size(), &g[0], 1) / aux; 
 			}
 			copy(xwork.begin(), xwork.end(), x.begin()); 
 			copy(gt.begin(), gt.end(), g.begin()); 
+			double frel = (f_old - f); 
 			f_old = f; 
 
 			success |= test_tol(gt, m_gtol, SUCCESS_GTOLA);
-			if (f != 0.0 &&  (fabs((f_old - f) / f) < m_ftolr))
+
+		
+			if (frel < m_ftolr)
 				success |= SUCCESS_FTOLR;  
+			else
+				cvinfo() << "improovement = " << frel << "\n"; 
 			
 		}else{
 			// recover best solution 
 			copy(x.begin(), x.end(), xwork.begin() ); 
-			m_step /= m_step_scale;
+			step /= m_step_scale;
 		}
-		success |= test_tol(g, m_xtol / m_step, SUCCESS_XTOLA); 
-		cvmsg() << "[" << iter << "]: f=" << f << " step=" << m_step << "\n"; 
+		success |= test_tol(g, m_xtol / step, SUCCESS_XTOLA); 
+		cvinfo() << "[" << iter << "]: f=" << f << " step=" << step << "\n"; 
 	}
 	if (iter == m_maxiter) 
 		cvwarn() << "Iteration stopped because maximum number of iterations was reached\n"; 
@@ -107,7 +114,7 @@ int CGDSQMinimizer::do_run(CDoubleVector& x)
 		cvmsg() << "Stop: GTOLA\n"; 
 		
 	if (success & SUCCESS_FTOLR) 
-		cvmsg() << "Stop: RTOLR\n"; 
+		cvmsg() << "Stop: FTOLR\n"; 
 
 	cvmsg() << "Stop:" << success << " with " << iter << " of " << m_maxiter << "iterations\n"; 
 
@@ -121,7 +128,8 @@ CGDSQMinimizerPlugin::CGDSQMinimizerPlugin():
 	m_ftolr(0.0), 
 	m_maxiter(100), 
 	m_start_step(0.1), 
-	m_step_scale(2.0)
+	m_step_scale(2.0), 
+	m_min_step(1e-6)
 {
 	add_parameter("maxiter", new CUIntParameter(m_maxiter, 1, numeric_limits<int>::max(), false, 
 						   "Stopping criterion: the maximum number of iterations")); 
@@ -141,7 +149,7 @@ CGDSQMinimizerPlugin::CGDSQMinimizerPlugin():
 CMinimizer *CGDSQMinimizerPlugin::do_create() const
 {
 	TRACE_FUNCTION; 
-	return new CGDSQMinimizer(m_start_step, m_step_scale, m_xtol, m_gtol, m_ftolr, m_maxiter); 
+	return new CGDSQMinimizer(m_start_step, m_step_scale, m_xtol, m_gtol, m_ftolr, m_maxiter, m_min_step); 
 }
 
 const std::string CGDSQMinimizerPlugin::do_get_descr() const
