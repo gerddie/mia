ec85a62b083f0d5759f833a939b68f3eeede8b68
Gert Wollny <gw.fossdev@gmail.com>
2011-11-29 13:08:33 +0100

    Add factory traits as a separate file

---
+++ mia/core/factory_trait.hh
@@ -0,0 +1,76 @@
+/* -*- mia-c++  -*-
+ *
+ * Copyright (c) Leipzig, Madrid 1999-2011 Gert Wollny
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef mia_core_factory_trait_hh
+#define mia_core_factory_trait_hh
+
+#include <mia/core/defines.hh>
+
+NS_MIA_BEGIN
+/**
+   \ingroup traits 
+   \brief Type trait to enable the use of a factory product as command 
+      line option 
+
+   Type trait that enables the use of the factory creation in commen line parsing. 
+   This trait needs to be specialized for all factories that are to be used 
+   utilizing the TCmdFactoryOption interface. 
+   \tparam T a class that can be created by a TFactory through the call to 
+    the method TFactoryPluginHandler::produce(const char *plugindescr) of the 
+    corresponding factory plugin handler. 
+    \sa TCmdFactoryOption
+ */
+template <class T> 
+class FactoryTrait {
+	/// the typetrait type if not defined properly 
+	typedef typename T::must_create_trait_using_FACTORY_TRAIT type; 
+}; 
+
+/**
+   \ingroup traits 
+
+   \brief Type trait to enable the use of a factory product as command 
+      line option 
+      
+   This trait specializes FactoryTrait for shared pointers.  
+ */
+template <class T> 
+class FactoryTrait<std::shared_ptr<T> >  {
+public: 
+	/// the typetrait type 
+	typedef typename FactoryTrait<T>::type type; 
+}; 
+
+/**
+   \ingroup traits 
+
+   Specialize the FactoryTrait template for the given TFactoryPluginHandler 
+*/
+#define FACTORY_TRAIT(F)			\
+	template <>				\
+	class FactoryTrait< F::Instance::ProductPtr::element_type >  {	\
+	public:					\
+	typedef F type;		\
+	}; 
+
+
+NS_MIA_END
+
+#endif
