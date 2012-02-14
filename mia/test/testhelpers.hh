66198cf4d22ebfab7ccf97c3d4a5a8e2d53c0246
Gert Wollny <gw.fossdev@gmail.com>
2012-02-14 13:20:04 +0100

    add some test comparison dispatch class

---
+++ mia/test/testhelpers.hh
@@ -0,0 +1,78 @@
+/* -*- mia-c++  -*-
+ *
+ * Copyright (c) Leipzig, Madrid 1999-2012 Gert Wollny
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef mia_test_testhelpers_hh
+#define mia_test_testhelpers_hh
+
+#include <typetraits>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+namespace miatest {
+
+namespace impl {
+
+template <typename T, bool is_integral> 
+struct __test_equal_or_close {
+	static void apply(T value, T expect, double, double) {
+		BOOST_CHECK_EQUAL(value, expect); 
+	}
+}; 
+
+template <typename T> 
+struct __test_equal_or_close<T, true> {
+	static void apply(T value, T expect, double reltol, double abstol) {
+		// handle the check against zero case: 
+		if (fabs(expect) > 1e-8) 
+			BOOST_CHECK_CLOSE(value, expect, reltol); 
+		else {
+			BOOST_CHECK(fabs(value - expect) < abstol); 
+		}
+	}
+};
+
+}
+
+/**
+   \ingroup tests 
+
+   \brief compare helper function for uni tests 
+    
+   This function is used to hide the uglyness of selecting the proper test method when 
+   comparing either integers or floating point values during tests. The function calls 
+   BOOST_CHECK_EQUAL for integral types, BOOST_CHECK_CLOSE, if the expected value is larger then 
+   1e-8, and BOOST_CHECK if the expected value is smaller then 1e-8 
+   \tparam T the type of the values to be compared 
+   \param value the value to test 
+   \param expect the expected value 
+   \param reltol relative tolerance to use if the type T is floating point and \a expect is larger then 1e-8
+   \param abstol  absolute tolerance to use if the type T is floating point and \a expect is smaller then 1e-8
+*/
+
+template <typename T> 
+void equal_or_close(T value, T expect, double reltol = 0.1, double abstol=1e-12) 
+{
+	impl::test_equal_or_close<T, std::is_integral<T> >::apple(*i, *t, reltol, abstol);
+}
+
+}
+
+#endif
+
