25dc789678b9672780db8e7bd25620a9cee4c74a
Gert Wollny <gw.fossdev@gmail.com>
2012-03-07 13:24:05 +0100

    test for the stylsheet in sgml and xml directory

--- doc/create_chunked_html.sh
+++ doc/create_chunked_html.sh
@@ -1,5 +1,15 @@
+#!/bin/sh 
+if [ -e   /usr/share/sgml/docbook/xsl-stylesheets/html/chunk.xsl ]; then 
+    stylsheet=/usr/share/sgml/docbook/xsl-stylesheets/html/chunk.xsl
+elif  [ -e  /usr/share/xml/docbook/xsl-stylesheets/html/chunk.xsl ]; then 
+    stylsheet=/usr/share/sgml/docbook/xsl-stylesheets/html/chunk.xsl
+else 
+   echo "dockbook stylesheet not found, did you install 'docbook-xsl'?"
+   exit 1
+fi 
+
 xsltproc --param chunk.section.depth 3 \
     --param chunk.first.sections 1 \
     --stringparam html.stylesheet  progref.css \
     --output userref/ \
-    /usr/share/sgml/docbook/xsl-stylesheets/html/chunk.xsl progref.xml
+    $stylesheet progref.xml
