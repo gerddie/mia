2322d52ba47b81e98c8ed9bc5f06d89d14e8dbe5
Gert Wollny <gw.fossdev@gmail.com>
2012-03-08 12:31:12 +0100

    rethink the revision tag use when recompiling

--- mia/core/revision.sh
+++ mia/core/revision.sh
@@ -3,33 +3,35 @@
 
 [ -e ~/.profile ] && . ~/.profile
 
-if [ -d $1/.git ]; then
-    git describe >/dev/null 2>&1 
-    if [ "x$?" = "x128" ] ; then 
-	rev=`git show | grep commit | sed -e "s/commit//"` 
-    else 
-	rev=`git describe`    
-    fi 
-    val="#define LIBMIA_REVISION "\"$rev\"
-    echo $val >revision.hh.new
-    if [ -e revision.hh ] ; then 
-       if diff revision.hh.new revision.hh >/dev/null 2>&1 ; then 
-	   rm -f revision_info.hh.new
-       else
-	   mv revision.hh.new revision.hh
-       fi
-    else 
-	   mv revision.hh.new revision.hh
-    fi 
-elif [ ! -f $1/mia/core/revision.hh ] ; then 
+if [ ! -f $1/mia/core/revision.hh ] ; then 
+    if [ -d $1/.git ]; then
+	git describe >/dev/null 2>&1 
+	if [ "x$?" = "x128" ] ; then 
+	    rev=`git show | grep commit | sed -e "s/commit//"` 
+	else 
+	    rev=`git describe`    
+	fi 
+	val="#define LIBMIA_REVISION "\"$rev\"
+	echo $val >revision.hh.new
+	if [ -e revision.hh ] ; then 
+	    if diff revision.hh.new revision.hh >/dev/null 2>&1 ; then 
+		rm -f revision_info.hh.new
+	    else
+		mv revision.hh.new revision.hh
+	    fi
+	else 
+	    mv revision.hh.new revision.hh
+	fi 
+    else
 # if we are not in git then the distribution hopefuilly contains a revision.hh file, if not
 # tell the user 
-    echo "WARNING: Unable to obtain revision info from git tree"
-    echo "WARNING: Either you're not using a working copy of the git version"
-    echo "WARNING: or the git  client is not available"
-    echo "WARNING: As a workaround you may create a 'revision.hh' by hand in the <build root>/mia/core directory"
-    echo "WARNING: with the line '#define LIBMIA_REVISION \"unknown\"'"
-    echo "WARNING: otherwise build will fail with 'revision.cc: revision.hh: No such file or directory'"
+	echo "WARNING: Unable to obtain revision info from git tree"
+	echo "WARNING: Either you're not using a working copy of the git version"
+	echo "WARNING: or the git  client is not available"
+	echo "WARNING: As a workaround you may create a 'revision.hh' by hand in the <build root>/mia/core directory"
+	echo "WARNING: with the line '#define LIBMIA_REVISION \"unknown\"'"
+	echo "WARNING: otherwise build will fail with 'revision.cc: revision.hh: No such file or directory'"
+    fi
 fi
 
 
