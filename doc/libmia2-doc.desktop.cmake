f895678834d9d6e0512d5b1d38a938cb8aca12f5
Gert Wollny <gw.fossdev@gmail.com>
2012-03-20 19:00:37 +0100

    update icons and desktop files

--- doc/libmia2-doc.desktop.cmake
+++ doc/libmia2-doc.desktop.cmake
@@ -2,6 +2,7 @@
 Name=MIA Programming Documentation
 Comment=The Doxygen generated documentation of the MIA library 
 Exec=xdg-open @FULL_DOC_INSTALL_PATH@/libdoc/index.html
+Icon=help-browser
 Terminal=false
 Type=Application
 Encoding=UTF-8
---
+++ doc/mia2-doc.desktop.cmake
@@ -0,0 +1,10 @@
+[Desktop Entry]
+Name=MIA User Reference
+Comment=The cross-references documentation of the command line tools provided by MIA
+Exec=xdg-open @FULL_DOC_INSTALL_PATH@/userref/index.html
+Icon=help-browser
+Terminal=false
+Type=Application
+Encoding=UTF-8
+Categories=Help;
+X-Desktop-File-Install-Version=0.18
