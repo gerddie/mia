9fff539cc08bb027d2c92cbef682016e1917d807
Gert Wollny <gw.fossdev@gmail.com>
2012-05-22 21:13:51 +0200

    create a test case that works

--- mia/2d/splinepenalty/test_divcurl.cc
+++ mia/2d/splinepenalty/test_divcurl.cc
@@ -22,6 +22,7 @@
 
 #include <mia/internal/plugintester.hh>
 #include <mia/2d/splinepenalty/divcurl.hh>
+#include <mia/2d/interpolator.hh>
 
 using namespace divcurl_splinepenalty; 
 NS_MIA_USE
@@ -31,23 +32,32 @@ CSplineKernelTestPath kernel_test_path;
 
 BOOST_AUTO_TEST_CASE( test_divcurl_cost ) 
 {
-	C2DDivcurlSplinePenalty  penalty(4.0, 6.0, 1.0); 
+	C2DDivcurlSplinePenalty  penalty(1.0, 1.0, 1.0); 
 
-	C2DBounds size(1,2); 
-	C2DFVector range(2,3); 
+	C2DBounds size(31,31); 
+	C2DFVector range(8,8); 
 	C2DFVectorfield coef(size); 
-	coef(0,0) = C2DFVector(1,2); 
-	coef(0,1) = C2DFVector(3,4);
 	
-	auto kernel = produce_spline_kernel("bspline:d=3"); 
+	double scale = range.x / (size.x - 1); 
+	double mid = (size.x - 1.0) / 2.0; 
+
+	auto ic = coef.begin(); 
+	for(unsigned int y = 0; y < size.y; ++y) {
+		double fy = (float(y) - mid) * scale; 
+		for(unsigned  x = 0; x < size.x; ++x, ++ic) {
+			double fx = (float(x) - mid) * scale;
+			double v = exp(- fx * fx - fy * fy); 
+			*ic = C2DFVector( fx * v, fy * v);
+		}
+	}
+	auto kernel = produce_spline_kernel("bspline:d=4"); 
+
+	T2DConvoluteInterpolator<C2DFVector> source(coef, kernel);
+	auto coeff_double = source.get_coefficients(); 
+	transform(coeff_double.begin(), coeff_double.end(), coef.begin(), 
+		  [](const C2DDVector& x) { return C2DFVector(x);}); 
 	
 	penalty.initialize(size, range, kernel); 
-	CDoubleVector gradient(6, true); 
-	
-	BOOST_CHECK_EQUAL(penalty.value(coef), 5.0); 
-	BOOST_CHECK_EQUAL(gradient[0], -2.0); 
-	BOOST_CHECK_EQUAL(gradient[1], -3.0); 
-	BOOST_CHECK_EQUAL(gradient[2], -2.0); 
-	BOOST_CHECK_EQUAL(gradient[3], -3.0); 
+	BOOST_CHECK_CLOSE(penalty.value(coef), 6 * M_PI, 0.5); 
 
 }
