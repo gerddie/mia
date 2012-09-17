9f066a1f99c35d57d36cb761de8a1a84e4227b1d
Gert Wollny <gw.fossdev@gmail.com>
2012-09-17 12:26:56 +0200

    correct test for evaluating gradinet along the boundaries

--- mia/3d/test_nfg.cc
+++ mia/3d/test_nfg.cc
@@ -31,6 +31,10 @@ using namespace std;
 
 const float check_noise_level = sqrt((499.0 - 131.0 * 131.0 / 54.0) / 53.0 );
 
+const float grad_sum_norm = 2+ 5+ 2 + sqrt(6.5)+ sqrt(9.25)+  2.5 + 3+ 4 + sqrt(21.25) + 2; 
+
+const float check_jump_level = check_noise_level * grad_sum_norm / 27.0f;
+
 P3DImage create_test_image()
 {
 	const float init_data[27] = { 0, 1, 0,
@@ -45,6 +49,22 @@ P3DImage create_test_image()
 				      7, 3, 0,
 				      2, 7, 6
 	};
+
+
+	/* the gradient: 
+	   (0,0,0), (0,0,0),(0,0,0); 
+	   (0,2,0),(-3,4,0),(0,2,0);     2, 5, 2 
+	   (0,0,0), (0,0,0), (0,0,0);
+	   
+	   (0,   0, 0),( 0,  0,  0), (0,  0, 0); 
+	   (0,2.5, .5),( 0,  3,-.5), (0,2.5, 0); sqrt(6.5), sqrt(9,25), 2.5
+	   (0,  0, -1),( 0,  0, -1), (0,  0, 1); 1, 1, 1
+
+	   (0,0,0), (0,0,0),  (0,0,0); 
+	   (0,1,0), (-3.5,3,0),(0,3,0); 1, sqrt(21.25), 3, 
+	   (0,0,0), (2,0,0),  (0,0,0); 2   
+
+	 */
 
 	/*
 	  0:  5 :   0 :   0
@@ -67,7 +87,7 @@ BOOST_AUTO_TEST_CASE( check_nfg_n )
 {
 	P3DImage image = create_test_image();
 
-	const float noise_level = 27.0 / sqrt(9.25);
+	const float noise_level = 27.0 / grad_sum_norm;
 	C3DFVectorfield ngf = get_nfg_n(*image,  noise_level);
 
 
@@ -91,7 +111,7 @@ BOOST_AUTO_TEST_CASE( check_nfg_j )
 {
 	P3DImage image = create_test_image();
 
-	const float jump_level = get_jump_level(*image, 27.0 / sqrt(9.25));
+	const float jump_level = get_jump_level(*image, 27.0 / grad_sum_norm);
 	C3DFVectorfield ngf = get_nfg_j(*image,  jump_level*jump_level);
 
 	C3DFVector center = ngf(1,1,1);
@@ -122,7 +142,7 @@ BOOST_AUTO_TEST_CASE( check_get_noise_level )
 BOOST_AUTO_TEST_CASE( check_intensity_jump_level )
 {
 	P3DImage image = create_test_image();
-	const float noise_level = 27.0 / sqrt(9.25);
+	const float noise_level = 27.0 / grad_sum_norm;
 
 	BOOST_CHECK_CLOSE(get_jump_level(*image, noise_level), 1.0f, 0.1);
 
@@ -131,6 +151,6 @@ BOOST_AUTO_TEST_CASE( check_intensity_jump_level )
 BOOST_AUTO_TEST_CASE(  test_get_jump_level2 )
 {
 	P3DImage image = create_test_image();
-	BOOST_CHECK_CLOSE(get_jump_level(*image), check_noise_level * sqrt(9.25f) / 27.0f , 0.1);
+	BOOST_CHECK_CLOSE(get_jump_level(*image), check_jump_level , 0.1);
 }
 
