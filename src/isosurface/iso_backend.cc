ffe8144fc17699ae0814f35ac5c6f3e4e35e24d5
Gert Wollny <gw.fossdev@gmail.com>
2012-10-09 17:48:24 +0200

    add program to extract an iso-surface from a stack of images

--- src/isosurface/iso_backend.cc
+++ src/isosurface/iso_backend.cc
@@ -21,9 +21,13 @@
 
 #include <gts.h>
 #include <mia/3d/3DImage.hh>
+#include <mia/2d/2dimageio.hh>
 #include <mia/core/filter.hh>
 
 using namespace mia; 
+using std::vector; 
+using std::string; 
+using std::invalid_argument; 
 
 class CImage2Double: public TFilter<C3DDImage> {
 public:	
@@ -48,6 +52,39 @@ static void iso_func(gdouble ** f, GtsCartesianGrid g, guint z, gpointer data)
 		for (size_t x = 0; x < g.nx; ++x,++p)
 			f[x][y] = *p;
 }
+
+
+struct FCopySlice: public TFilter<void> {
+	FCopySlice(double **f):m_f(f){}; 
+	
+	template <typename T> 
+	void operator()(const T2DImage<T>& image) const {
+		auto p = image.begin(); 
+		for (size_t y = 0; y < image.get_size().y; y++)	
+			for (size_t x = 0; x < image.get_size().x; ++x,++p)
+				m_f[x][y] = *p;
+	}
+	double **m_f; 
+}; 
+	
+static void iso_func_from_slices(gdouble ** f, GtsCartesianGrid g, guint z, gpointer data)
+{
+	const vector<string> *slices = reinterpret_cast<const vector<string> *>(data); 
+
+        auto slice_image = load_image2d((*slices)[z]); 
+	
+	if (slice_image->get_size().x != g.nx ||
+	    slice_image->get_size().y != g.ny ) 
+		throw create_exception<invalid_argument>("Input slice of wrong size, got ", slice_image->get_size(), 
+							 ", expect <", g.nx, ", ", g.ny, ">"); 
+	
+	
+	cvmsg() << "extracting ..." << (100 * (z+1))/g.nz << "%\r";  
+
+	FCopySlice cs(f); 
+	mia::filter(cs, *slice_image); 
+}
+
 
 static void iso_func_b(gdouble ** f, GtsCartesianGrid g, guint z, gpointer data)
 {
@@ -90,21 +127,21 @@ typedef struct {
 } stop_info_t;
 
 
-static gboolean stop_nedges (gdouble cost, guint number, stop_info_t *stop_info)
+static gboolean stop_nedges (gdouble MIA_PARAM_UNUSED(cost), guint number, stop_info_t *stop_info)
 {
 	g_return_val_if_fail (stop_info != NULL, TRUE);
 	return (number < (guint)stop_info->max_edges);
 }
 
 
-static gboolean stop_nfaces(gdouble cost, guint nedges, stop_info_t *stop_info) 
+static gboolean stop_nfaces(gdouble MIA_PARAM_UNUSED(cost), guint MIA_PARAM_UNUSED(nedges), stop_info_t *stop_info) 
 {
 	g_return_val_if_fail (stop_info != NULL, TRUE);
 
 	return ( gts_surface_face_number(stop_info->surface) < (guint)stop_info->max_faces);
 }
 
-static gboolean stop_cost(gdouble cost, guint nedges, gdouble *max_cost) 
+static gboolean stop_cost(gdouble cost, guint MIA_PARAM_UNUSED(nedges), gdouble *max_cost) 
 {
 	g_return_val_if_fail (max_cost != NULL, TRUE);
 	return (cost > *max_cost);
@@ -166,7 +203,7 @@ static gboolean stop_nedges_verbose (gdouble cost, guint number, stop_info_t *st
 }
 
 
-static int stop_nfaces_verbose(gdouble cost, guint nedges, stop_info_t *stop_info) 
+static int stop_nfaces_verbose(gdouble cost, guint MIA_PARAM_UNUSED(nedges), stop_info_t *stop_info) 
 {
 	size_t number;
 	
@@ -207,7 +244,7 @@ static int stop_nfaces_verbose(gdouble cost, guint nedges, stop_info_t *stop_inf
 			 "Remaining: %02.0f:%02.0f:%02.0f",
 			 stop_info->cost_name, 
 			 cost,
-			 number, 
+			 static_cast<unsigned int>(number), 
 			 100.*(stop_info->nmax - number)/(stop_info->nmax - stop_info->max_faces),
 			 (stop_info->nold - number)/g_timer_elapsed (stop_info->timer, NULL),
 			 hours, mins, secs,
@@ -244,9 +281,37 @@ static int stop_cost_verbose(gdouble cost, guint nedges, gdouble *max_cost)
 	return FALSE; 	
 }
 
-void do_revert(GtsTriangle *t, gpointer *data) 
+void do_revert(GtsTriangle *t, gpointer *MIA_PARAM_UNUSED(data)) 
 {
 	gts_triangle_revert(t);
+}
+
+static GtsSurface *extract_surface(const vector<string>& slices, gdouble iso_value, bool MIA_PARAM_UNUSED(use_border))
+{
+	GtsCartesianGrid g;	
+	GtsSurface *surface; 
+
+	auto prototype = load_image2d(slices[0]); 
+	
+	
+	C2DFVector vox = prototype->get_pixel_size(); 
+	g.dx  = vox.x; 
+	g.dy  = vox.y; 
+	g.dz  = 1; 
+	cvmsg() << "voxel = " << g.dx <<","<< g.dy <<","<< g.dz << '\n';
+
+	g.x = 0.0; g.nx = prototype->get_size().x; 
+	g.y = 0.0; g.ny = prototype->get_size().y; 
+	g.z = 0.0; g.nz = slices.size();
+
+	surface = gts_surface_new (gts_surface_class (), gts_face_class (), gts_edge_class (),
+				   gts_vertex_class ());
+	
+	if (surface)
+		gts_isosurface_tetra(surface, g, iso_func_from_slices, (gpointer)&slices, iso_value);
+	gts_surface_foreach_face(surface,(GtsFunc)do_revert,0);
+	
+	return surface; 
 }
 
 static GtsSurface *extract_surface(const C3DImage& src, gdouble iso_value, bool use_border)
@@ -287,39 +352,19 @@ static GtsSurface *extract_surface(const C3DImage& src, gdouble iso_value, bool 
 	return surface; 
 }
 
-/** This function creates a GTS triangle mesh as the iso-surface of a given input image.
-    The mesh is afterwards optimized by first collapsing small edges, and then by smallest volume change
-    \param src the input image
-    \param iso_value the iso value for surface extraction 
-    \param max_edges mesh optimization stops if number of edges is less the this value; -1 = unlimited, overrides all other stop criteria
-    \param max_faces mesh optimization stops if number of faces is less the this value; -1 = unlimited, overrides cost criterium
-    \param max_cost mesh optimization stops if the cost of an edge collaps is above, no method switch is applied; -1 unlimited 
-    \param bordered create a bounding box around the image to ensure a closed surface
-    \param coarsen_method_factor switch from the first edge collapsing function to the latter at this factor of max edges/faces from targeted one
-    \param verbose give some output
-    \returns an GtsSurface
-*/
-
-
-
-
-GtsSurface *iso_surface(const C3DImage& src, gfloat iso_value, gint max_edges, gint max_faces, 
-			gdouble max_cost, gboolean bordered, 
-			gfloat coarsen_method_factor)
+static GtsSurface *iso_surface_optimize(GtsSurface *mesh, gint max_edges, gint max_faces, 
+			gdouble max_cost, gfloat coarsen_method_factor)
 {
 	bool verbose = cverb.get_level() <= vstream::ml_message;
-	
-     	stop_info_t stop_info ={ NULL, -1, -1,0,0, NULL, NULL};
+     	stop_info_t stop_info ={ NULL, -1, -1,0,0, NULL, NULL, NULL};
 	gdouble fold = M_PI/180.;
 	GtsVolumeOptimizedParams params = { 0.5, 0.5, 0. };
 	
 	stop_info.max_faces = max_faces; 
 	stop_info.max_edges = max_edges; 
 	stop_info.cost_name = "volume change"; 
-	
-	stop_info.surface = extract_surface(src, iso_value, bordered);
-	
-	
+	stop_info.surface = mesh; 
+
 	if (!stop_info.surface) {
 		cverr()<<"Unable to generate surface" << '\n'; 
 		return NULL; 
@@ -332,7 +377,7 @@ GtsSurface *iso_surface(const C3DImage& src, gfloat iso_value, gint max_edges, g
 	
 	
 	if (stop_info.max_edges > 0) {
-		stop_info_t stop_info_first ={NULL, -1, -1, 0, 0, NULL, NULL};
+		stop_info_t stop_info_first ={NULL, -1, -1, 0, 0, NULL, NULL, NULL};
 		
 		stop_info_first.surface = stop_info.surface; 
 		stop_info_first.max_edges = int(coarsen_method_factor * stop_info.max_edges);
@@ -358,7 +403,7 @@ GtsSurface *iso_surface(const C3DImage& src, gfloat iso_value, gint max_edges, g
 	else if (stop_info.max_faces >0 ) {
 		
 		
-		stop_info_t stop_info_first ={NULL, -1, -1,0,0,NULL,NULL};
+		stop_info_t stop_info_first ={NULL, -1, -1,0,0,NULL,NULL, NULL};
 
 		stop_info_first.surface = stop_info.surface; 
 		stop_info_first.max_faces  = int(coarsen_method_factor * stop_info.max_faces);
@@ -392,8 +437,41 @@ GtsSurface *iso_surface(const C3DImage& src, gfloat iso_value, gint max_edges, g
 	if (verbose)
 		gts_surface_print_stats(stop_info.surface, stderr);
 	
-	return stop_info.surface;
-	}
+	return stop_info.surface;	
+		
+}
+
+/** This function creates a GTS triangle mesh as the iso-surface of a given input image.
+    The mesh is afterwards optimized by first collapsing small edges, and then by smallest volume change
+    \param src the input image
+    \param iso_value the iso value for surface extraction 
+    \param max_edges mesh optimization stops if number of edges is less the this value; -1 = unlimited, overrides all other stop criteria
+    \param max_faces mesh optimization stops if number of faces is less the this value; -1 = unlimited, overrides cost criterium
+    \param max_cost mesh optimization stops if the cost of an edge collaps is above, no method switch is applied; -1 unlimited 
+    \param bordered create a bounding box around the image to ensure a closed surface
+    \param coarsen_method_factor switch from the first edge collapsing function to the latter at this factor of max edges/faces from targeted one
+    \param verbose give some output
+    \returns an GtsSurface
+*/
+GtsSurface *iso_surface(const C3DImage& src, gfloat iso_value, gint max_edges, gint max_faces, 
+			gdouble max_cost, gboolean bordered, 
+			gfloat coarsen_method_factor)
+{
+
+	GtsSurface *mesh = extract_surface(src, iso_value, bordered);
+	
+	return iso_surface_optimize(mesh, max_edges, max_faces, max_cost, coarsen_method_factor); 
+}
+
+GtsSurface *iso_surface(const vector<string>& slices, gfloat iso_value, gint max_edges, gint max_faces, 
+			gdouble max_cost, gboolean bordered, 
+			gfloat coarsen_method_factor)
+{
+
+	GtsSurface *mesh = extract_surface(slices, iso_value, bordered);
+	
+	return iso_surface_optimize(mesh, max_edges, max_faces, max_cost, coarsen_method_factor); 
+}
 
 
 
---
+++ src/isosurface/iso_from_slices.cc
@@ -0,0 +1,122 @@
+/* -*- mia-c++  -*-
+**
+** Copyright (C) 1999 Max-Planck-Institute of Cognitive Neurosience
+**                    Gert Wollny <wollnyAtcbs.mpg.de>
+**  
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU Lesser Public License as published by
+** the Free Software Foundation; either version 2.1 of the License, or
+** (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Lesser Public License for more details.
+** 
+** You should have received a copy of the GNU Lesser Public License
+** along with this program; if not, write to the Free Software 
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+
+#include <mia/mesh/triangularMesh.hh>
+#include <mia/3d/3DImage.hh>
+#include <mia/3d/3dimageio.hh>
+
+#include <mia/core/cmdlineparser.hh>
+#include <mia/internal/main.hh>
+#include <gts.h>
+
+using namespace std; 
+using namespace mia;
+
+
+GtsSurface *iso_surface(const vector<string>& src, gfloat iso_value, gint max_edges, gint max_faces, 
+			gdouble max_cost, gboolean bordered, 
+			gfloat coarsen_method_factor);
+
+
+CTriangleMesh *gts_to_mona_mesh(GtsSurface *surface);
+
+const SProgramDescription g_description = {
+        {pdi_group, "Analysis, filtering, combining, and segmentation of 3D images"}, 
+	{pdi_short, "Extract an ist-surface froma a 3D image."}, 
+	{pdi_description, "This program is used to extract an iso-surface from the input gray scale "
+	 "image given as slices."},
+	{pdi_example_descr, "Extract the surface from an image set with numbers imageXXXX.png that "
+	 "corresponds to  the value 30 and stop optimizing when the mesh consists of less than 100000 "
+	 "triangles."}, 
+	{pdi_example_code, "-i image0000.png -o mesh.vmesh -s 30 -f 100000"  }
+}; 
+
+
+int do_main (int argc, char * argv[])
+{
+	
+	string in_filename; 
+	string out_filename; 
+
+	float iso_value = 128.0f; 
+	bool use_border = false; 
+	
+	gint max_faces = -1; 
+	gint max_edges = -1; 
+	gfloat  max_cost = 0.1; 
+	gfloat factor = 4.0; 
+
+	CCmdOptionList options(g_description);
+	options.set_group("File-IO"); 
+	options.add(make_opt(  in_filename, "in-image", 'i', "input image filename pattern", CCmdOption::required )); 
+	options.add(make_opt(  out_filename, "out-mesh", 'o', "output mesh", CCmdOption::required )); 
+
+	options.set_group("Image options"); 
+	options.add(make_opt(  iso_value, "iso-value", 's', "iso-value of iso surface to be extracted")); 
+	//	options.add(make_opt(  use_border, "bordered", 'b', "put an empty border around the image to ensure a closed surface")); 
+	
+	options.set_group("Mesh optimization"); 
+	options.add(make_opt(  max_faces, "max-faces", 'f', "maximum number of Faces,")); 
+	options.add(make_opt(  max_edges, "max-edges", 'e', "maximum number of Edges")); 
+	options.add(make_opt(  max_cost, "max-cost", 'c', "maximum cost for edge collaps")); 
+	options.add(make_opt(  factor, "ratio", 'r', "ratio of faces(edges) to target number, at which  "
+			       "the optimization changes from edge-length to volume-optimized"));
+	
+	
+	if (options.parse(argc, argv) != CCmdOptionList::hr_no) 
+		return EXIT_SUCCESS; 
+	
+	
+	size_t start_filenum = 0;
+	size_t end_filenum  = 0;
+	size_t format_width = 0;
+	
+	string src_basename = get_filename_pattern_and_range(in_filename, start_filenum, end_filenum, format_width);
+	if (start_filenum >= end_filenum)
+		throw invalid_argument(string("no files match pattern ") + src_basename);
+	
+	
+	vector<string> slices; 
+	slices.reserve(end_filenum - start_filenum); 
+	for (size_t i = start_filenum; i < end_filenum; ++i)
+		slices.push_back(create_filename(src_basename.c_str(), i));
+		
+	GtsSurface *surface = iso_surface(slices, iso_value, max_edges, max_faces, 
+					  max_cost, use_border, factor);
+	
+	if (surface) {
+		unique_ptr<CTriangleMesh> mesh(gts_to_mona_mesh(surface));
+		gts_object_destroy((GtsObject*)surface);
+		
+		if ( !CMeshIOPluginHandler::instance().save(out_filename, *mesh) ) {
+			throw runtime_error("Unable to write mesh to " + out_filename);
+		}
+	}else {
+		throw runtime_error("Unable to creat iso-surface."); 
+	}
+	return EXIT_SUCCESS; 
+}
+
+MIA_MAIN(do_main); 
