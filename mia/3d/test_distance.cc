fb8c9b3882d2532a6913a081e9396f6d9cbc560d
Gert Wollny <gw.fossdev@gmail.com>
2012-10-15 18:39:28 +0200

    Refactor tests

--- mia/3d/test_distance.cc
+++ mia/3d/test_distance.cc
@@ -25,120 +25,76 @@
 using namespace mia; 
 
 
-BOOST_AUTO_TEST_CASE( test_distance_full3d_inf ) 
+struct Distance3DInfFixture {
+	static const bool src_init[64]; 
+	static const float test_val[64]; 
+}; 
+
+struct Distance3DFuncFixture {
+	static const float src_init[64]; 
+	static const float test_val[64]; 
+}; 
+
+
+
+ 
+
+BOOST_FIXTURE_TEST_CASE( test_distance_full3d_inf, Distance3DInfFixture ) 
 {
-	bool src[64] = {
-		1, 0, 0, 0,   
-		0, 0, 0, 0,   
-		0, 0, 0, 0,  
-		0, 0, 0, 0, 
-		
-		0, 0, 0, 0,   
-		0, 0, 0, 0,   
-		0, 0, 1, 0,  
-		0, 0, 0, 0, 
-
-		0, 0, 0, 0,   
-		0, 1, 0, 0,   
-		0, 0, 0, 0,  
-		0, 0, 0, 0,
-		
-		0, 0, 0, 0,   
-		0, 0, 0, 0,   
-		0, 1, 0, 0,  
-		0, 0, 0, 0
-	}; 
-	
-	float test[64] = {
-		0, 1, 4, 6,   
-		1, 2, 2, 3,
-		4, 2, 1, 2,  
-		6, 3, 2, 3, 
-
-		1, 2, 3, 5,   
-		2, 1, 1, 2,   
-		3, 1, 0, 1,  
-		5, 2, 1, 2, 
-		
-		2, 1, 2, 5,   
-		1, 0, 1, 3,   
-		2, 1, 1, 2,  
-		3, 2, 2, 3, 
-
-		3, 2, 3, 6,   
-		2, 1, 2, 5,   
-		1, 0, 1, 4,  
-		2, 1, 2, 5
-	}; 
-	
 	C3DFImage src_img(C3DBounds(4,4,4)); 
 	
-	distance_transform_prepare(&src[0], &src[64], src_img.begin()); 
+	distance_transform_prepare(&src_init[0], &src_init[64], src_img.begin()); 
 	
 	C3DFImage result =  distance_transform(src_img); 
 
 	int k = 0; 
 	for(auto i = result.begin(); i != result.end(); ++i, ++k ) {
-		BOOST_CHECK_CLOSE(*i, test[k], 0.1); 
+		BOOST_CHECK_CLOSE(*i, test_val[k], 0.1); 
 	}
 }
 
 
-BOOST_AUTO_TEST_CASE( test_distance_full3d_func ) 
+BOOST_FIXTURE_TEST_CASE( test_distance_full3d_func,  Distance3DFuncFixture ) 
 {
-	float src[64] = {
-		1, 2, 4, 5,   
-		5, 4, 3, 2,   
-		3, 2, 2, 3,  
-		4, 5, 3, 1, 
-		
-		1, 4, 5, 2,   
-		1, 2, 3, 3,   
-		2, 3, 1, 4,  
-		4, 4, 3, 4, 
-
-		3, 2, 1, 1,   
-		2, 1, 4, 3,   
-		0, 3, 2, 1,  
-		5, 4, 4, 2,
-		
-		7, 8, 1, 2,   
-		6, 5, 4, 3,   
-		4, 1, 4, 2,  
-		5, 5, 4, 3
-	}; 
-	
-	float test[64] = {
-		1, 2, 5, 5,   
-		2, 3, 3, 4,   
-		3, 3, 2, 2,  
-	        5, 4, 2, 1, 
-		
-		1, 2, 2, 2,   
-		1, 2, 2, 3,   
-		1, 2, 1, 2,  
-		2, 3, 2, 2, 
-
-		2, 2, 1, 1,   
-		1, 1, 2, 2,   
-		0, 1, 2, 1,  
-		1, 2, 3, 2,
-		
-		4, 2, 1, 2,   
-		2, 2, 2, 3,   
-		1, 1, 2, 2,  
-		2, 2, 3, 3
-	}; 
-	
 	C3DFImage src_img(C3DBounds(4,4,4)); 
 	
-	distance_transform_prepare(&src[0], &src[64], src_img.begin()); 
+	distance_transform_prepare(&src_init[0], &src_init[64], src_img.begin()); 
 	
 	C3DFImage result =  distance_transform(src_img); 
 
 	int k = 0; 
 	for(auto i = result.begin(); i != result.end(); ++i, ++k ) {
-		cvdebug() << "k=" << k << ":" << *i << ", vs " << test[k] << "\n"; 
-		BOOST_CHECK_CLOSE(*i, test[k], 0.1); 
+		cvdebug() << "k=" << k << ":" << *i << ", vs " << test_val[k] << "\n"; 
+		BOOST_CHECK_CLOSE(*i, test_val[k], 0.1); 
 	}
 }
+
+
+const bool Distance3DInfFixture::src_init[] = {
+		1, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,  0, 0, 0, 0,    
+		0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 1, 0,  0, 0, 0, 0, 
+		0, 0, 0, 0,   0, 1, 0, 0,   0, 0, 0, 0,  0, 0, 0, 0,
+		0, 0, 0, 0,   0, 0, 0, 0,   0, 1, 0, 0,  0, 0, 0, 0
+	}; 
+
+
+const float Distance3DInfFixture::test_val[] = {
+		0, 1, 4, 6,   1, 2, 2, 3,   4, 2, 1, 2,   6, 3, 2, 3,   
+		1, 2, 3, 5,   2, 1, 1, 2,   3, 1, 0, 1,   5, 2, 1, 2,  
+		2, 1, 2, 5,   1, 0, 1, 3,   2, 1, 1, 2,   3, 2, 2, 3,   
+		3, 2, 3, 6,   2, 1, 2, 5,   1, 0, 1, 4,   2, 1, 2, 5
+}; 
+
+const float Distance3DFuncFixture::src_init[] = {
+	1, 2, 4, 5,   5, 4, 3, 2,   3, 2, 2, 3,  4, 5, 3, 1, 
+	1, 4, 5, 2,   1, 2, 3, 3,   2, 3, 1, 4,  4, 4, 3, 4, 
+	3, 2, 1, 1,   2, 1, 4, 3,   0, 3, 2, 1,  5, 4, 4, 2,
+	7, 8, 1, 2,   6, 5, 4, 3,   4, 1, 4, 2,  5, 5, 4, 3
+}; 
+	
+const float Distance3DFuncFixture::test_val[] = {
+	1, 2, 5, 5,   2, 3, 3, 4,   3, 3, 2, 2,  5, 4, 2, 1, 
+	1, 2, 2, 2,   1, 2, 2, 3,   1, 2, 1, 2,  2, 3, 2, 2, 
+	2, 2, 1, 1,   1, 1, 2, 2,   0, 1, 2, 1,  1, 2, 3, 2,
+	4, 2, 1, 2,   2, 2, 2, 3,   1, 1, 2, 2,  2, 2, 3, 3
+}; 
