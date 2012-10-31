35c7e42a0fdace27b17b258d8505e7abdaae3574
Gert Wollny <gw.fossdev@gmail.com>
2012-10-31 18:18:49 +0100

    add test to see if all attributes are reordered

--- mia/mesh/filter/test_vtxsort.cc
+++ mia/mesh/filter/test_vtxsort.cc
@@ -33,8 +33,14 @@ protected:
 	void test_expected(const CTriangleMesh& changed, const CTriangleMesh& test) const;
 	
 	typedef CTriangleMesh::CTrianglefield::value_type Triangle;
+	typedef CTriangleMesh::CColorfield::value_type Color;
 	CTriangleMesh mesh; 
 }; 
+
+class MeshVtxsortWithAttrFixture : public MeshVtxsortFixture{
+protected:
+	MeshVtxsortWithAttrFixture(); 
+};
 
 BOOST_FIXTURE_TEST_CASE( test_simple_mesh_z_order, MeshVtxsortFixture) 
 {
@@ -53,7 +59,6 @@ BOOST_FIXTURE_TEST_CASE( test_simple_mesh_z_order, MeshVtxsortFixture)
 	
 	test_expected(*filtered_mesh, test);
 }
-
 
 BOOST_FIXTURE_TEST_CASE( test_simple_mesh_y_order, MeshVtxsortFixture) 
 {
@@ -109,6 +114,36 @@ BOOST_FIXTURE_TEST_CASE( test_simple_mesh_comb, MeshVtxsortFixture)
 	test_expected(*filtered_mesh, test);
 }
 
+BOOST_FIXTURE_TEST_CASE( test_mesh_z_order_with_attr, MeshVtxsortWithAttrFixture) 
+{
+	auto testv = CTriangleMesh::PVertexfield(new CTriangleMesh::CVertexfield({
+				C3DFVector(3,4,1), C3DFVector(5,3,3), 
+					C3DFVector(2,1,4), C3DFVector(1,5,8)})); 
+
+	auto testt = CTriangleMesh::PTrianglefield(new CTriangleMesh::CTrianglefield({
+				Triangle(3,1,0), Triangle(1,0,2), 
+					Triangle(3,0,2), Triangle(3,2,1)})); 
+
+	auto testn = CTriangleMesh::PNormalfield(new CTriangleMesh::CNormalfield({
+				C3DFVector(0,0,3), C3DFVector(0,0,2), 
+				 C3DFVector(0,0,4), C3DFVector(0,0,1)
+					})); 
+
+	auto testc = CTriangleMesh::PColorfield(new CTriangleMesh::CColorfield({
+				Color(1,0,0), Color(0,1,0), Color(1,1,0) , Color(0,0,1)
+					})); 
+
+	auto tests = CTriangleMesh::PScalefield(new CTriangleMesh::CScalefield({3,2,4,1})); 
+	
+	CTriangleMesh test(testt, testv, testn, testc, tests); 
+
+	auto scale = BOOST_TEST_create_from_plugin<CVtxSortMeshFilterPlugin>("vtxsort:dir=[<0,0,1>]");
+	auto filtered_mesh = scale->filter(mesh); 
+	
+	test_expected(*filtered_mesh, test);
+}
+
+
 MeshVtxsortFixture::MeshVtxsortFixture():mesh(4,4)
 {
 	
@@ -120,6 +155,19 @@ MeshVtxsortFixture::MeshVtxsortFixture():mesh(4,4)
 		Triangle(0,1,2), Triangle(1,2,3), Triangle(0,2,3), Triangle(0,3,1)}; 
 	copy(vertices.begin(), vertices.end(), mesh.vertices_begin()); 
 	copy(triangles.begin(), triangles.end(), mesh.triangles_begin());
+}
+
+MeshVtxsortWithAttrFixture::MeshVtxsortWithAttrFixture()
+{
+	CTriangleMesh::CNormalfield nfield({C3DFVector(0,0,1), C3DFVector(0,0,2), 
+				C3DFVector(0,0,3), C3DFVector(0,0,4)}); 
+	CTriangleMesh::CColorfield cfield({Color(0,0,1), Color(0,1,0), Color(1,0,0), Color(1,1,0)});
+	CTriangleMesh::CScalefield sfield({1,2,3,4}); 
+	
+	copy(nfield.begin(), nfield.end(), mesh.normals_begin()); 
+	copy(cfield.begin(), cfield.end(), mesh.color_begin()); 
+	copy(sfield.begin(), sfield.end(), mesh.scale_begin()); 
+
 }
 
 void MeshVtxsortFixture::test_expected(const CTriangleMesh& changed, const CTriangleMesh& test) const
