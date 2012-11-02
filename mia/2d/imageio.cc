a86bafadb4d85936a7f338d103a58c0ceda16a8c
Gert Wollny <gw.fossdev@gmail.com>
2012-11-02 13:25:12 +0100

    rename files in 2D to remove the 2D part from the filenames

---
+++ mia/2d/imageio.cc
@@ -0,0 +1,207 @@
+/* -*- mia-c++  -*-
+ *
+ * Copyright (c) Leipzig, Madrid 1999-2012 Gert Wollny
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <mia/core/export_handler.hh>
+
+#include <mia/2d/imageio.hh>
+#include <mia/core/ioplugin.cxx>
+#include <mia/core/iohandler.cxx>
+#include <mia/core/errormacro.hh>
+#include <mia/core/tools.hh>
+
+NS_MIA_BEGIN
+
+const char *io_2dimage_type::data_descr = "2dimage";
+
+C2DImageVector *C2DImageVector::clone() const
+{
+	return new C2DImageVector(*this);
+}
+
+C2DImageIOPPH::C2DImageIOPPH()
+{
+	C2DFVectorTranslator::register_for("pixel");
+}
+
+
+template <> const char *  const 
+TPluginHandler<C2DImageIOPlugin>::m_help =  
+   "These plug-ins handle loading and storing of 2D images to the supported image file types.";
+
+
+P2DImageVector EXPORT_2D create_image2d_vector(P2DImage image)
+{
+	P2DImageVector out_list_ptr(new C2DImageIOPluginHandler::Instance::Data);
+	out_list_ptr->push_back(image);
+	return out_list_ptr;
+}
+
+P2DImage  EXPORT_2D load_image2d(const std::string& filename)
+{
+	C2DImageIOPluginHandler::Instance::PData  in_image_list =
+		C2DImageIOPluginHandler::instance().load(filename);
+
+	if (!in_image_list || in_image_list->empty()) {
+		throw create_exception<runtime_error>( filename, " no image  found");
+	}
+	if (in_image_list->size() > 1) {
+		cvwarn() << "load_image2d:'" << filename
+			 << "' more then one image found, loading only first one\n";
+	}
+
+	return *in_image_list->begin();
+}
+
+bool  EXPORT_2D save_image(const std::string& filename, P2DImage image)
+{
+	C2DImageIOPluginHandler::Instance::Data out_image_list;
+	out_image_list.push_back(image);
+	return C2DImageIOPluginHandler::instance().save(filename, out_image_list);
+}
+
+bool  EXPORT_2D save_image(const std::string& filename, C2DImage& image)
+{
+	return save_image(filename, P2DImage(&image, void_destructor<C2DImage>())); 
+}
+
+C2DImageGroupedSeries  EXPORT_2D load_image_series(const std::vector<std::string>& filenames, 
+						   CProgressCallback *cb)
+{
+	typedef map<int, C2DImageSeries> C2DImageSeriesGroupHelper; 
+	typedef map<int, C2DImageSeriesGroupHelper> C2DGroupSeriesHelper; 
+	typedef map<string, C2DGroupSeriesHelper> C2DImageGroupedSeriesHelper; 
+
+
+	C2DImageGroupedSeriesHelper collector; 
+	const static string unknown_protocol("Unknown"); 
+	
+	int step = 0; 
+	if (cb) 
+		cb->set_range(filenames.size()); 
+
+	cvdebug() << "Load " << filenames.size() << "  images\n"; 
+	for (auto f = filenames.begin(); f != filenames.end(); ++f, ++step) {
+		// give some feedback 
+		cvdebug() << "Load " << *f << "\n"; 
+		if (cb && ! (step & 0x1f) )
+			cb->update(step); 
+		
+		C2DImageIOPluginHandler::Instance::PData  in_image_list =
+			C2DImageIOPluginHandler::instance().load(*f);
+		if (!in_image_list) {
+			cverr() << "load_image_series: File '" << *f 
+				<< "' doesn't provide images this software can read\n"; 
+			continue; 
+		}
+		for (auto i = in_image_list->begin(); i != in_image_list->end(); ++i) {
+			// look for protocol attribute 
+			auto protocol = (*i)->get_attribute_as_string(IDProtocolName); 
+			std::string key = protocol.empty() ? unknown_protocol : protocol;
+			if (collector.find(key) == collector.end())
+				collector[key] = C2DGroupSeriesHelper(); 
+			C2DGroupSeriesHelper& group = collector[key]; 
+
+			// look for series number 
+			auto seriesnr = (*i)->get_attribute(IDSeriesNumber);
+			const CIntAttribute *psn = dynamic_cast<const CIntAttribute *>(seriesnr.get());
+			int saqnr = -1; 
+			if (psn) 
+				saqnr = *psn; 
+
+			if (group.find(saqnr) == group.end())
+				group[saqnr] = C2DImageSeriesGroupHelper(); 
+			C2DImageSeriesGroupHelper& subgroup = group[saqnr]; 
+
+			// look for acquisition number 
+			auto attr = (*i)->get_attribute(IDAcquisitionNumber); 
+			const CIntAttribute *paq = dynamic_cast<const CIntAttribute *>(attr.get());
+			int aqnr = -1; 
+			if (paq) 
+				aqnr = *paq; 
+			if (subgroup.find(aqnr) == subgroup.end()) 
+				subgroup[aqnr] = C2DImageSeries(); 
+			
+			// look for slice location number, if non exists fake one
+			attr = (*i)->get_attribute(IDSliceLocation);
+			const CFloatAttribute *pinst = dynamic_cast<const CFloatAttribute *>(attr.get());
+			float location; 
+			if (!pinst) {
+				location = 0.0; 
+				(*i)->set_attribute(IDSliceLocation, PAttribute(new CFloatAttribute(location))); 
+			} else 
+				location = floor(1000.0 * *pinst) / 1000.0; 
+			
+			subgroup[aqnr].push_back(*i); 
+			cvinfo() << "Add '" <<* f  
+				  << "' to Protocol group '" << protocol
+				  << "' with acquisition no. " << aqnr 
+				  << "' and location no. " << location << "\n"; 
+		}
+	}
+	// now sort the slices and move the groups to a zero-index based vector 
+	C2DImageGroupedSeries result; 
+	for (auto g = collector.begin(); g != collector.end(); ++g) {
+		cvinfo() << "Protocol '" << g->first << "'\n"; 
+		C2DImageSeriesGroup group; 
+		for (auto saq = g->second.begin(); saq != g->second.end(); ++saq) {
+			for (auto aq = saq->second.begin(); aq != saq->second.end(); ++aq) {
+				sort(aq->second.begin(), aq->second.end(), 
+				     [](const P2DImage& lhs, const P2DImage& rhs) {
+					     const auto lhs_attr = lhs->get_attribute(IDSliceLocation);
+					     const auto rhs_attr = rhs->get_attribute(IDSliceLocation);
+					     const float lhs_inr = dynamic_cast<const CFloatAttribute&>(*lhs_attr); 
+					     const float rhs_inr = dynamic_cast<const CFloatAttribute&>(*rhs_attr); 
+					     return lhs_inr < rhs_inr; 
+				     }); 
+				cvinfo() << "  Acquisition " << aq->first << " with "<< aq->second.size() <<" slices\n"; 
+				group.push_back(aq->second); 
+			}
+			result[g->first] = group; 
+		}
+	}
+	return result; 
+}
+
+C2DImageIOPluginHandlerTestPath::C2DImageIOPluginHandlerTestPath()
+{
+	CPathNameArray searchpath({bfs::path(".")});
+	C2DImageIOPluginHandler::set_search_path(searchpath);
+}
+
+EXPORT_2D const char * IDAcquisitionDate =   "AcquisitionDate";
+EXPORT_2D const char * IDImageType =         "ImageType";
+EXPORT_2D const char * IDAcquisitionNumber = "AcquisitionNumber";
+EXPORT_2D const char * IDInstanceNumber =    "InstanceNumber";
+EXPORT_2D const char * IDSliceLocation = "SliceLocation";
+EXPORT_2D const char * IDSeriesNumber = "SeriesNumber";
+EXPORT_2D const char * IDModality =          "Modality";
+EXPORT_2D const char * IDPatientOrientation ="PatientOrientation";
+EXPORT_2D const char * IDPatientPosition = "PatientPosition";
+EXPORT_2D const char * IDSmallestImagePixelValue = "SmallestImagePixelValue";
+EXPORT_2D const char * IDLargestImagePixelValue = "LargestImagePixelValue";
+EXPORT_2D const char * IDStudyID = "StudyID";
+EXPORT_2D const char * IDProtocolName = "ProtocolName"; 
+
+template class TIOPlugin<io_2dimage_type>;
+template class THandlerSingleton<C2DImageIOPPH>;
+template class TIOPluginHandler<C2DImageIOPlugin>;
+template class TPluginHandler<C2DImageIOPlugin>;
+
+NS_MIA_END
---
+++ mia/2d/imageiotest.cc
@@ -0,0 +1,197 @@
+/* -*- mia-c++  -*-
+ *
+ * Copyright (c) Leipzig, Madrid 1999-2012 Gert Wollny
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <climits>
+
+#include <boost/test/unit_test_suite.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <mia/core/msgstream.hh>
+#include <mia/core/history.hh>
+#include <mia/2d/imageio.hh>
+#include <mia/core/filter.hh>
+
+
+using namespace std;
+using namespace boost;
+using namespace boost::unit_test;
+
+NS_MIA_BEGIN
+typedef C2DImageIOPluginHandler::Instance::Interface Interface;
+
+
+
+
+class CImageCompare: public TFilter<bool>  {
+public:
+	template <typename  T>
+	CImageCompare::result_type operator()(const T2DImage<T>& a, const T2DImage<T>& b) const
+	{
+		return equal(a.begin(), a.end(), b.begin());
+	}
+};
+
+static bool is_equal(const C2DImage& a, const C2DImage& b)
+{
+	BOOST_REQUIRE(a.get_pixel_type() == b.get_pixel_type());
+	BOOST_REQUIRE(a.get_size() == b.get_size());
+
+	CImageCompare compare;
+
+	return filter_equal(compare, a, b);
+}
+
+static void test_attributes_equal(const CAttributedData& a, const CAttributedData& b)
+{
+	BOOST_CHECK(a == b);
+}
+
+static void check_save_load(const C2DImageVector& images, const Interface& imgio)
+{
+	const string format = imgio.get_name();
+	const string tmp_name = string("imgtest") + string(".") + format;
+
+	cvdebug() << format << ":" << tmp_name << endl;
+
+	BOOST_REQUIRE(imgio.save(tmp_name.c_str(), images));
+
+	cvdebug() << format << "saved" << std::endl;
+
+	std::shared_ptr<C2DImageVector > reread(imgio.load(tmp_name.c_str()));
+
+	BOOST_REQUIRE(reread.get());
+	cvdebug() << images.size() << " vs. " << reread->size() << "\n";
+	BOOST_REQUIRE(images.size() == reread->size());
+
+	C2DImageVector::const_iterator in_i = images.begin();
+	C2DImageVector::const_iterator in_e = images.end();
+
+	C2DImageVector::const_iterator lo_i = reread->begin();
+
+	for (; in_i != in_e; ++in_i, ++lo_i) {
+		BOOST_REQUIRE(is_equal(**in_i, **lo_i));
+		if (imgio.has_property(io_plugin_property_has_attributes)) {
+			test_attributes_equal(**in_i, **lo_i);
+		}
+
+	}
+
+	unlink(tmp_name.c_str());
+	cvdebug() << tmp_name << " unlinked\n";
+}
+
+
+template <typename T>
+static P2DImage create_image()
+{
+	T2DImage<T> *r = new T2DImage<T>(C2DBounds(13,19));
+	int k = 0;
+	for(typename T2DImage<T>::iterator i = r->begin(); i != r->end(); ++i, ++k)
+		*i = T(k);
+
+	r->set_pixel_size(C2DFVector(1.5f, 4.6f));
+	r->set_attribute("some", PAttribute(new TAttribute<string>("some text")));
+
+	return  P2DImage(r);
+}
+
+template <>
+P2DImage create_image<bool>()
+{
+	C2DBitImage *r = new C2DBitImage(C2DBounds(13,19));
+	for(C2DBitImage::iterator i = r->begin(); i != r->end(); ++i)
+#ifdef WIN32
+			*i =  rand() > RAND_MAX/2;
+#else
+			*i =  drand48() > 0.5;
+#endif
+
+	r->set_pixel_size(C2DFVector(1.5f, 4.6f));
+	r->set_attribute("some", PAttribute(new TAttribute<string>("some other text")));
+	return  P2DImage(r);
+}
+
+static P2DImage create_image_of_type(EPixelType type)
+{
+	switch (type) {
+	case it_bit:    return create_image<bool>();
+	case it_sbyte:  return create_image<signed char>();
+	case it_ubyte:  return create_image<unsigned char>();
+	case it_sshort: return create_image<signed short>();
+	case it_ushort: return create_image<unsigned short>();
+	case it_sint:   return create_image<signed int>();
+	case it_uint:   return create_image<unsigned int>();
+#ifdef LONG_64BIT
+	case it_slong:  return create_image<signed long>();
+	case it_ulong:  return create_image<unsigned long>();
+#endif
+	case it_float:  return create_image<float>();
+	case it_double: return create_image<double>();
+	default:
+		BOOST_FAIL("IO plugin supports more types then we know of");
+		return P2DImage();
+	}
+}
+
+
+
+static void test_multi(const Interface & plugin)
+{
+	C2DImageVector src_list;
+	Interface::PixelTypeSet pixeltypes = plugin.supported_pixel_types();
+
+	for(set<EPixelType>::const_iterator i = pixeltypes.begin();
+	    i != pixeltypes.end(); ++i) {
+		src_list.push_back(create_image_of_type(*i));
+	}
+	check_save_load(src_list, plugin);
+}
+
+static void test_single(const Interface & plugin)
+{
+	cvdebug() << "test_imageio_plugin_single:" << plugin.get_name() << "\n";
+
+	Interface::PixelTypeSet pixeltypes = plugin.supported_pixel_types();
+
+	for(set<EPixelType>::const_iterator i = pixeltypes.begin();
+	    i != pixeltypes.end(); ++i) {
+		C2DImageVector src_list;
+		src_list.push_back(create_image_of_type(*i));
+		check_save_load(src_list, plugin);
+	}
+
+	cvdebug() << "test_imageio_plugin_single:" << plugin.get_name() << " done\n";
+}
+
+void EXPORT_2DTEST test_2dimageio_plugins()
+{
+	const C2DImageIOPluginHandler::Instance& plugins = C2DImageIOPluginHandler::instance();
+
+	for (C2DImageIOPluginHandler::Instance::const_iterator i = plugins.begin();
+	     i != plugins.end(); ++i) {
+
+		if (i->second->has_property(io_plugin_property_multi_record))
+			test_multi(*i->second);
+		else
+			test_single(*i->second);
+	}
+}
+
+NS_MIA_END
