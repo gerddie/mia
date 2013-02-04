f2f23f5c972faa4a96557d40e3c947508b1a7abd
Gert Wollny <gw.fossdev@gmail.com>
2013-02-04 15:55:57 +0100

    Correct the preferred suffix specification.

--- addons/vistaio/3dvfvistaio.cc
+++ addons/vistaio/3dvfvistaio.cc
@@ -42,6 +42,7 @@ public:
 private:
 	PData do_load(const string& fname) const;
 	bool do_save(const string& fname, const Data& data) const;
+	std::string do_get_preferred_suffix() const; 
 	const string do_get_descr() const;
 };
 
@@ -53,6 +54,11 @@ CVista3DVFIOPlugin::CVista3DVFIOPlugin():
 	add_suffix(".vf");
 	add_suffix(".V");
 	add_suffix(".VF");
+}
+
+std::string CVista3DVFIOPlugin::do_get_preferred_suffix() const
+{
+	return "vf"; 
 }
 
 
@@ -132,6 +138,7 @@ public:
 private:
 	PData do_load(const string& fname) const;
 	bool do_save(const string& fname, const Data& data) const;
+	std::string do_get_preferred_suffix() const; 
 	const string do_get_descr() const;
 	template <typename T>
 	CScaled3DVFIOPlugin::PData read_compressed(const T3DVector<VistaIOLong>& _size, const C3DFVector& scale, 
@@ -143,8 +150,8 @@ CScaled3DVFIOPlugin::CScaled3DVFIOPlugin():
 	C3DVFIOPlugin("cvista")
 {
 	add_supported_type(it_float);
-	add_suffix(".svf");
-	add_suffix(".SVF");
+	add_suffix(".cvf");
+	add_suffix(".CVF");
 
 }
 
@@ -165,6 +172,12 @@ CScaled3DVFIOPlugin::PData CScaled3DVFIOPlugin::read_compressed(const T3DVector<
 	}
 	return result; 
 }
+
+std::string CScaled3DVFIOPlugin::do_get_preferred_suffix() const
+{
+	return "cvf"; 
+}
+
 
 CScaled3DVFIOPlugin::PData CScaled3DVFIOPlugin::do_load(const string& fname) const
 {
@@ -223,7 +236,7 @@ bool CScaled3DVFIOPlugin::do_save(const string& /*fname*/, const Data& /*data*/)
 
 const string CScaled3DVFIOPlugin::do_get_descr() const
 {
-	return "a 3d vector field io plugin for compressend vista";
+	return "obsolete 3d vector field io plugin for range compressend vista (saving not supported)";
 }
 
 extern "C" EXPORT  CPluginBase *get_plugin_interface()
