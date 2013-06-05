fe09b6c2acd9bf9afa1dc1e76cf97462d848cafa
Gert Wollny <gw.fossdev@gmail.com>
2013-06-05 13:45:50 +0200

    add a 3D image load plugin, closes #3

---
+++ mia/3d/filter/load.cc
@@ -0,0 +1,61 @@
+/* -*- mia-c++  -*-
+ *
+ * This file is part of MIA - a toolbox for medical image analysis 
+ * Copyright (c) Leipzig, Madrid 1999-2013 Gert Wollny
+ *
+ * MIA is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MIA; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <mia/3d/filter/load.hh>
+#include <mia/3d/imageio.hh>
+
+NS_BEGIN( load_3dimage_filter)
+
+NS_MIA_USE; 
+
+C3DLoad::C3DLoad(const std::string& name):
+	m_name(name)
+{
+}
+
+mia::P3DImage C3DLoad::do_filter(const mia::C3DImage& MIA_PARAM_UNUSED(image)) const
+{
+	return load_image3d(m_name); 
+	
+}
+
+C3DLoadFilterPluginFactory::C3DLoadFilterPluginFactory(): 
+	C3DFilterPlugin("load")
+{
+	add_parameter("file", new CStringParameter(m_filename, true,
+						   "name of the input file to load from.", 
+						   &C3DImageIOPluginHandler::instance()));
+}
+
+mia::C3DFilter *C3DLoadFilterPluginFactory::do_create()const
+{
+	return new C3DLoad(m_filename); 
+}
+
+const std::string C3DLoadFilterPluginFactory::do_get_descr()const
+{
+	return "Load the input image from a file and use it to replace the current image in the pipeline."; 
+}
+
+extern "C" EXPORT CPluginBase *get_plugin_interface()
+{
+	return new C3DLoadFilterPluginFactory();
+}
+NS_END
---
+++ mia/3d/filter/test_load.cc
@@ -0,0 +1,62 @@
+/* -*- mia-c++  -*-
+ *
+ * This file is part of MIA - a toolbox for medical image analysis 
+ * Copyright (c) Leipzig, Madrid 1999-2013 Gert Wollny
+ *
+ * MIA is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MIA; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <mia/internal/plugintester.hh>
+#include <mia/3d/filter/load.hh>
+#include <mia/core/datapool.hh>
+#include <mia/3d/imageio.hh>
+
+NS_MIA_USE
+using namespace std;
+using namespace ::boost;
+using namespace ::boost::unit_test;
+using namespace load_3dimage_filter;
+
+struct LoadFixture {
+	LoadFixture(); 
+	~LoadFixture(); 
+	
+
+	C3DUBImage *orig; 
+	P3DImage image; 
+}; 
+
+LoadFixture::LoadFixture()
+{
+	const unsigned char init[4] = {1,2,3,4}; 
+	orig = new C3DUBImage(C3DBounds(2,2,1), init); 
+	image.reset(orig); 
+        save_image("test.@", image); 
+}
+
+LoadFixture::~LoadFixture()
+{
+	boost::any dummy = CDatapool::instance().get_and_remove("test.@");
+}
+	
+
+BOOST_FIXTURE_TEST_CASE( test_3dfilter_load, LoadFixture )
+{
+        C3DUBImage dummy(C3DBounds(3,2,1)); 
+        
+	auto t = BOOST_TEST_create_from_plugin<C3DLoadFilterPluginFactory>("load:file=test.@");
+	auto loaded = t->filter(image); 
+	BOOST_CHECK(*image == *loaded);
+}
