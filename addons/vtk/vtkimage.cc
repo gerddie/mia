05a20bb990d4b3dbd4f21e1be4bbe85854578c12
Gert Wollny <gw.fossdev@gmail.com>
2013-12-12 16:34:22 +0100

    eliminate some compiler warnings

--- addons/vtk/vtkimage.cc
+++ addons/vtk/vtkimage.cc
@@ -97,8 +97,7 @@ C3DImage *read_image(const C3DBounds& size, void *scalars)
 {
 	cvdebug() << "VTK/MetaIO read image of type " <<  __type_descr<T>::value << "\n"; 
 
-	typedef typename __vtk_data_array<T>::type myDataArray; 
-	
+
 	const T *my_scalars = reinterpret_cast<const T *>(scalars); 
 	if (!my_scalars) 
 		throw create_exception<logic_error>("CVtk3DImageIOPlugin::load: input image scalar type bogus"); 
