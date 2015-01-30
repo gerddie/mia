de59fcff9bc2b1a35e5c8c2eb459897fee304142
Gert Wollny <gw.fossdev@gmail.com>
2015-01-30 16:12:00 +0100

    add SOR skeleton

---
+++ mia/3d/vfregularizer/sor.cc
@@ -0,0 +1,125 @@
+/* -*- mia-c++  -*-
+ *
+ * This file is part of MIA - a toolbox for medical image analysis 
+ * Copyright (c) Leipzig, Madrid 1999-2015 Gert Wollny
+ *
+ * MIA is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MIA; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <mia/3d/vfregularizer/sor.hh>
+
+NS_MIA_BEGIN
+
+C3DSORVectorfieldRegularizer::C3DSORVectorfieldRegularizer(float abs_epsilon, float rel_epsilon, 
+                                                           unsigned maxiter, 
+                                                           P3DVectorfieldRegularizerKernel kernel):
+        m_abs_epsilon(abs_epsilon), 
+        m_rel_epsilon(rel_epsilon), 
+        m_maxiter(maxiter), 
+        m_kernel(kernel)
+{
+}
+
+double C3DSORVectorfieldRegularizer::do_run(C3DFVectorfield& velocity, C3DFVectorfield& force, const C3DFVectorfield& deform) const
+{
+        m_kernel->set_data_fields(velocity, force);
+
+        unsigned padding = m_kernel->get_boundary_padding(); 
+
+        unsigned iter = 0; 
+        float residuum = 0.0; 
+        float first_rel_residuum = -1.0; 
+
+        unsigned work_size_z = velocity.get_size().z - padding;  
+        unsigned work_size_y = velocity.get_size().y - padding;  
+        
+        do {
+                iter++; 
+
+                residuum == 0.0f; 
+
+                auto buffers = m_kernel->get_buffers(); 
+                for (unsigned z = padding; z < work_size_z; ++z) {
+                        m_kernel->start_slice(z, *buffers);
+                        for (unsigned y = padding; y < work_size_y; ++y) {
+                                residuum += m_kernel->evaluate_row(y, z, *buffers);
+                        }
+                }
+
+                if (first_rel_residuum < 0) 
+                        first_rel_residuum = m_rel_epsilon * residuum; 
+                
+                
+
+        } while (iter < m_maxiter && 
+                 residuum > m_abs_epsilon && 
+                 residuum > first_rel_residuum); 
+        
+        float max_pert = 0.0f; 
+        if (m_kernel->has_pertuberation()) {
+                m_kernel->set_data_fields(velocity, deform);
+                auto buffers = m_kernel->get_buffers(); 
+                for (unsigned z = padding; z < work_size_z; ++z) {
+                        m_kernel->start_slice(z, *buffers);
+                        for (unsigned y = padding; y < work_size_y; ++y) {
+                                float pert = m_kernel->evaluate_pertuberation_row(y, z, *buffers);
+                                if (max_pert < pert) 
+                                        max_pert = pert; 
+                        }
+                }
+        }else{
+                // find maximum in velocity field 
+                for (v : velocity) {
+                        float pert = v.norm2(); 
+                        if (max_pert < pert) 
+                                max_pert = pert; 
+                }
+        }
+        return sqrt(max_pert);
+}
+
+C3DSORVectorfieldRegularizerPlugin::C3DSORVectorfieldRegularizerPlugin():
+        C3DFVectorfieldRegularizerPlugin("sor"), 
+        m_abs_epsilon(0.01), 
+        m_rel_epsilon(1e-5), 
+        m_maxiter(100)
+{
+}
+        
+
+C3DFVfFluidStandardRegularizerKernel *C3DSORVectorfieldRegularizerPlugin::do_create() const
+{
+        add_parameter("rel_f", new mia::CFloatParameter(m_rel_epsilon, 1e-10, 1.0, false, 
+                                                        "breaking condition: relative residuum"));  
+        add_parameter("abs_f", new mia::CFloatParameter(m_abs_epsilon, 1e-10, 1.0, false, 
+                                                        "breaking condition: absolute residuum"));  
+        add_parameter("maxiter", new mia::CUIntParameter(m_maxiter, 1, 3000000, false, "maximum number of iterations"));  
+        
+        // there should be some method to select the arch optimized version 
+        // 
+        add_parameter(make_param(m_kernel, "fluid-generic", "solver kernel to be used")); 
+
+        return new C3DSORVectorfieldRegularizer(m_abs_epsilon, m_rel_epsilon, 
+                                                m_maxiter, m_kernel); 
+}
+        
+const std::string C3DSORVectorfieldRegularizerPlugin::do_get_descr() const
+{
+        return "This plugin implements successive (over-)relaxation as a solver "
+                "to regularize the vector field."; 
+}
+
+
+NS_MIA_END
---
+++ mia/3d/vfregularizer/sor.hh
@@ -0,0 +1,66 @@
+/* -*- mia-c++  -*-
+ *
+ * This file is part of MIA - a toolbox for medical image analysis 
+ * Copyright (c) Leipzig, Madrid 1999-2015 Gert Wollny
+ *
+ * MIA is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MIA; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef mia_3d_vfregularizer_sor_hh
+#define mia_3d_vfregularizer_sor_hh
+
+#include <mia/3d/vfregularizer.hh>
+
+NS_MIA_BEGIN
+
+class C3DSORVectorfieldRegularizer : public C3DFVectorfieldRegularizer {
+      
+public: 
+        C3DSORVectorfieldRegularizer(float abs_epsilon, float rel_epsilon, 
+                                     unsigned maxiter, 
+                                     P3DVectorfieldRegularizerKernel kernel);
+        
+private: 
+        double do_run(C3DFVectorfield& velocity, C3DFVectorfield& force, const C3DFVectorfield& deform) const; 
+        
+        float m_abs_epsilon;
+        float m_rel_epsilon; 
+        unsigned m_maxiter; 
+        P3DVectorfieldRegularizerKernel m_kernel; 
+                
+};  
+
+class C3DSORVectorfieldRegularizerPlugin : public C3DFVectorfieldRegularizerPlugin {
+
+public: 
+        C3DSORVectorfieldRegularizerPlugin(); 
+        
+private:
+        
+        C3DFVfFluidStandardRegularizerKernel *do_create() const;
+        
+        const std::string do_get_descr() const;
+
+
+        float m_abs_epsilon;
+        float m_rel_epsilon; 
+        unsigned m_maxiter; 
+        P3DVectorfieldRegularizerKernel m_kernel; 
+        
+}; 
+
+NS_MIA_END
+
+#endif 
