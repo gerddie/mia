bdb09639334e068cca6db820c7ad4ea510ca4df4
Gert Wollny <gw.fossdev@gmail.com>
2015-02-03 16:21:19 +0100

    Correct visibility for template instanciation

--- mia/3d/vectorfield.hh
+++ mia/3d/vectorfield.hh
@@ -85,6 +85,7 @@ public:
 	
 };
 
+extern template class EXPORT_3D T3DVectorfield<C3DFVector>;
 
 class EXPORT_3D C3DFVectorfield : public T3DVectorfield<C3DFVector> {
 public: 
@@ -98,7 +99,7 @@ public:
 	
 };
 
-extern template class T3DVectorfield<C3DFVector>;
+
 extern template class EXPORT_3D T3DVectorfield<C3DDVector>;
 extern template class EXPORT_3D range3d_iterator<T3DDatafield<C3DFVector>::iterator>;
 extern template class EXPORT_3D range3d_iterator_with_boundary_flag<T3DDatafield<C3DFVector>::iterator>;
