89a64f6749fd69787cbc26b37f442f950632c4f1
Gert Wollny <gw.fossdev@gmail.com>
2015-06-17 15:16:10 +0200

    correct counters

--- mia/3d/filter/reorient.cc
+++ mia/3d/filter/reorient.cc
@@ -198,7 +198,7 @@ C3DReorient::result_type C3DReorient::operator () (const mia::T3DImage<T>& data)
 		for (size_t z = 0; z < data.get_size().z; ++z) {
 			for (size_t y = 0; y < data.get_size().y; ++y) {
 				unsigned  ox = data.get_size().x - 1; 
-				for (size_t x = 0; x < data.get_size().x; ++x) {
+				for (size_t x = 0; x < data.get_size().x; ++x, --ox) {
 					(*result)(z,y,ox) = data(x,y,z);
 				}
 				
