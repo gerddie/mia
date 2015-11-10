ad5f4710c7fb04e091bdbd75e4f3953e74929631
Gert Wollny <gw.fossdev@gmail.com>
2015-11-10 20:15:26 +0100

    parallize mean filter

--- mia/2d/filter/mean.cc
+++ mia/2d/filter/mean.cc
@@ -22,6 +22,9 @@
 #include <mia/2d/filter/mean.hh>
 #include <boost/type_traits/is_floating_point.hpp>
 
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+
 NS_BEGIN(mean_2dimage_filter)
 NS_MIA_USE;
 using namespace std;
@@ -106,12 +109,17 @@ C2DMean::result_type C2DMean::operator () (const T2DImage<T>& data) const
 	T2DImage<T> *tresult = new T2DImage<T>(data.get_size(), data);
 	P2DImage result(tresult);
 
-	typename T2DImage<T>::iterator i = tresult->begin();
 
-	for (size_t y = 0; y < data.get_size().y; ++y)
-		for (size_t x = 0; x < data.get_size().x; ++x, ++i)
-			*i = __dispatch_filter<T, is_floating_point>::apply(data, x, y, m_hw);
-
+	int hw = m_hw; 
+        auto run_line  = [hw, data, tresult](const tbb::blocked_range<size_t>& range) {
+		for (auto y = range.begin(); y !=  range.end(); ++y) {
+			typename T2DImage<T>::iterator i = tresult->begin_at(0, y);
+			for (size_t x = 0; x < data.get_size().x; ++x, ++i)
+				*i = __dispatch_filter<T, is_floating_point>::apply(data, x, y, hw);
+		}
+	};
+	tbb::parallel_for(tbb::blocked_range<size_t>(0, data.get_size().y, 1), run_line);
+	
 	return result;
 }
 
