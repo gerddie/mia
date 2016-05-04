91f4cf251f1b0daa09f0fc559b15d703d6d8d6a6
Gert Wollny <gw.fossdev@gmail.com>
2016-05-04 16:08:20 +0200

    Add a shape output test

--- mia/2d/creator/test_circle.cc
+++ mia/2d/creator/test_circle.cc
@@ -36,7 +36,6 @@ typedef boost::mpl::vector<bool,
                            unsigned short,
                            signed int,
                            unsigned int,
-                           float,
                            double
 #ifdef HAVE_INT64
                            mia_int64,
@@ -48,17 +47,73 @@ typedef boost::mpl::vector<bool,
 BOOST_AUTO_TEST_CASE_TEMPLATE( test_circle_create, T , type_list )
 {
         auto ccreator = BOOST_TEST_create_from_plugin<C2DCircleCreatorPlugin>("circle:f=2,p=3");
-
-        C2DBounds size(16,32); 
+        
+        C2DBounds size(16,16); 
         P2DImage image = (*ccreator)(size, pixel_type<T>::value);
-
+        
         const T2DImage<T>& cc = dynamic_cast<const T2DImage<T>&>(*image); 
+        
+        BOOST_CHECK_EQUAL(cc.get_size(), size);
+}
+
+/// test the pattern only once, for the float case
+BOOST_AUTO_TEST_CASE( test_circle_create_float )
+{
+        auto ccreator = BOOST_TEST_create_from_plugin<C2DCircleCreatorPlugin>("circle:f=2,p=3");
+	
+        C2DBounds size(16,16); 
+        P2DImage image = (*ccreator)(size, it_float);
+	
+        const C2DFImage& cc = dynamic_cast<const C2DFImage&>(*image); 
 
         BOOST_CHECK_EQUAL(cc.get_size(), size);
 
-        // check the pattern
+	vector<float> result= {
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 
+		0, 0, 0, 0, 0, 0, 0, 0.995185,
+		0.989177, 0.995185, 0, 0, 0, 0, 0, 0, 
+		0, 0, 0, 0, 1, -0.24298, -0.92388, -0.998795,
+		-1, -0.998795, -0.92388, -0.24298, 1, 0, 0, 0, 
+		0, 0, 0, 0, -0.24298, -0.881921, -0.14673, 0.19509,
+		0.24298, 0.19509, -0.14673, -0.881921, -0.24298, 0, 0, 0, 
+		0, 0, 0, 0, -0.92388, -0.14673, 0.707107, 0.903989,
+		0.92388, 0.903989, 0.707107, -0.14673, -0.92388, 0, 0, 0, 
+		0, 0, 0, 0.995185, -0.998795, 0.19509, 0.903989,
+		0.995185, 0.998795, 0.995185, 0.903989, 0.19509, -0.998795, 0.995185, 0, 0, 
+		0, 0, 0, 0.989177, -1, 0.24298, 0.92388, 0.998795,
+		1, 0.998795, 0.92388, 0.24298, -1, 0.989177, 0, 0, 
+		0, 0, 0, 0.995185, -0.998795, 0.19509, 0.903989, 0.995185,
+		0.998795, 0.995185, 0.903989, 0.19509, -0.998795, 0.995185, 0, 0, 
+		0, 0, 0, 0, -0.92388, -0.14673, 0.707107, 0.903989, 0.92388,
+		0.903989, 0.707107, -0.14673, -0.92388, 0, 0, 0, 
+		0, 0, 0, 0, -0.24298, -0.881921, -0.14673, 0.19509, 0.24298,
+		0.19509, -0.14673, -0.881921, -0.24298, 0, 0, 0, 
+		0, 0, 0, 0, 1, -0.24298, -0.92388, -0.998795, -1,
+		-0.998795, -0.92388, -0.24298, 1, 0, 0, 0, 
+		0, 0, 0, 0, 0, 0, 0, 0.995185, 0.989177,
+		0.995185, 0, 0, 0, 0, 0, 0, 
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	}; 
 
-        
+	auto ic = cc.begin();
+	auto ec = cc.end();
+	auto ir = result.begin();
+
+	while (ic != ec) {
+
+		if (fabs(*ir) > 1e-5)
+			BOOST_CHECK_CLOSE(*ic, *ir, 0.1);
+		else
+			BOOST_CHECK_SMALL(*ic, 1e-5f);
+		
+		++ic; ++ir; 
+	}; 
 }
 
 
